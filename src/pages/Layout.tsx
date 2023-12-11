4f8e2168ea92620cb51d650795594106eff10995
hookor <gerrard1219@gmail.com>
2023-12-11 10:56:06 +0900

    Feat: centralise layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import { styled } from 'styled-system/jsx';
+
 const Layout = () => {
   return (
     <>
-      <Suspense>
-        <Outlet />
-      </Suspense>
+      <Container>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </Container>
     </>
   );
 };
+
+const Container = styled.main`
+  max-width: 500px;
+`;
 
 export default Layout;
