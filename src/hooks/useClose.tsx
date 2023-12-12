bf06ef99819ac18844a932e5c4baab9b949c15f0
hookor <gerrard1219@gmail.com>
2023-12-12 10:10:54 +0900

    Feat: Header navigation hooks

---
+++ src/hooks/useClose.tsx
@@ -0,0 +1,20 @@
+import { useNavigateTo } from '@/hooks/useNavigateTo';
+
+export const useClose = () => {
+  const location = window.location.href;
+  const routeMap = {
+    post: '/posts',
+    register: '/home',
+    // PROFILE => 유저 본인으로 이동할 수 있게 수정
+    mypage: '/profile',
+    default: '/'
+  };
+
+  const handleTouch = () => {
+    const routesMap = Object.keys(routeMap);
+    const targetRoute =
+      routesMap.find(key => location.includes(key)) || routeMap['default'];
+    useNavigateTo(targetRoute);
+  };
+  return { handleTouch };
+};
