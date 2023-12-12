ca6be1dd5e69cbd0ec3ca7dcc4ef076cc07c33aa
swon95 <shimtkddnjs13@gmail.com>
2023-12-12 13:29:55 +0900

    Feat: Profile Detail Component

---
+++ src/constants/Profile.ts
@@ -0,0 +1,19 @@
+export const userName = {
+    loginName: 'zzzz'
+}
+
+export const userNickName = {
+    user1: '김감자'
+}
+
+export const userEmail = {
+    eMail: 'asd@asdasd.com'
+}
+
+export const addedCoffee = {
+    cafein: 1111
+}
+
+export const brand = {
+    brand1: '바나프레소'
+}
