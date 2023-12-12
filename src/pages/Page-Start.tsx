5070846e64a827559c6ea4fffd5f2f5f3862c2bc
Siwoo Lee <siwoo28@kakao.com>
2023-12-12 14:05:07 +0900

    Feat: signIn component

--- src/pages/Page-Start.tsx
+++ src/pages/Page-Start.tsx
@@ -1,7 +1,18 @@
+import { useRecoilValue } from 'recoil';
+import SignIn from '@/components/start/SignIn';
+import InitialForm from '@/components/start/InitialForm';
+import { authState } from '@/atoms/atoms';
 import { useShowFooter } from '@/hooks/useShowFooter';
 const Start = () => {
   useShowFooter(false);
-  return <>START</>;
+  const { initialized, signIn } = useRecoilValue(authState);
+
+  return (
+    <div>
+      {!signIn && <SignIn />}
+      {signIn && !initialized && <InitialForm />}
+    </div>
+  );
 };
 
 export default Start;
