07f75c477beea0e357a7b2f0d5774d1fa6b4419c
Siwoo Lee <siwoo28@kakao.com>
2023-12-12 14:06:23 +0900

    Design: signIn component design

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,1 +1,19 @@
- 
+import { css, cx } from 'styled-system/css';
+
+export const ButtonFont = css`
+  color: #202020;
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+export const SignInBtn = cx(
+  ButtonFont,
+  css`
+    width: 335px;
+    height: 54px;
+    border-radius: 16px;
+    font-size: var(--font-sizes-base);
+    line-height: 20px;
+  `
+);
