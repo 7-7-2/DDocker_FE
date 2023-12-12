685a5fb4aad2192fab48a0006148cf4c6840ad9b
swon95 <shimtkddnjs13@gmail.com>
2023-12-12 13:28:57 +0900

    Feat: FollowCount Component

--- src/pages/Page-Follow.tsx
+++ src/pages/Page-Follow.tsx
@@ -1,5 +1,43 @@
+import { FOLLOW } from '@/constants/Follow';
+import { styled } from 'styled-system/jsx';
+import { Flex, FlexCenter } from '@/styles/layout';
+
 const Follow = () => {
-  return <>FOLLOW</>;
+  return (
+    <Container className={FlexCenter}>
+      <Stat className={Flex}>
+        <StatLabel>팔로잉</StatLabel>
+        <StatLabel>팔로워</StatLabel>
+      </Stat>
+      <Stat className={Flex}>
+        <StatNumber>{FOLLOW.following}</StatNumber>
+        <StatNumber>{FOLLOW.followed}</StatNumber>
+      </Stat>
+    </Container>
+  );
 };
 
 export default Follow;
+
+const Container = styled.div`
+  justify-content: space-around;
+  padding: 20px;
+  width: 200px;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Stat = styled.div`
+  text-align: center;
+  flex-direction: row;
+`;
+
+const StatLabel = styled.span`
+  font-weight: bold;
+  margin: 0px 10px;
+`;
+
+const StatNumber = styled.span`
+  font-size: 0.8rem;
+  margin: 0px 18px;
+`;
