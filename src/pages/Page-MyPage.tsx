a794ca49fdb6bfe06d9f381fbbffe1ea1cc11117
swon95 <shimtkddnjs13@gmail.com>
2023-12-12 13:33:58 +0900

    Feat: Profile Img Component

--- src/pages/Page-MyPage.tsx
+++ src/pages/Page-MyPage.tsx
@@ -1,8 +1,13 @@
+import MyProfile from '@/components/mypage/MyProfile';
 import { useComposeHeader } from '@/hooks/useComposeHeader';
 
 export const MyPage = () => {
   useComposeHeader(false, ' 프로필 수정', 'icons');
-  return <>MYPAGE</>;
+  return (
+    <>
+      <MyProfile />
+    </>
+  );
 };
 
 export default MyPage;
