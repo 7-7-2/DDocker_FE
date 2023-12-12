da68a21ea4a8de7004bde0fd8a1eb87db43865a9
hookor <gerrard1219@gmail.com>
2023-12-12 10:09:29 +0900

    Feat: props and types

---
+++ src/constants/icons.ts
@@ -0,0 +1,12 @@
+export const ICON_SEARCH = {
+  id: 'icon-search',
+  size: '24'
+};
+export const ICON_NOTIFICATION = {
+  id: 'icon-notification',
+  size: '24'
+};
+export const ICON_CLOSE = {
+  id: 'icon-close',
+  size: '24'
+};
