b3daec330c9bbec796a4940c3f0d75e9999fb623
swon95 <shimtkddnjs13@gmail.com>
2023-12-12 13:29:55 +0900

    Feat: Profile Detail Component

--- src/components/profile/ProfileDetail.tsx
+++ src/components/profile/ProfileDetail.tsx
@@ -1,5 +1,42 @@
+import { styled } from 'styled-system/jsx';
+import { userName, addedCoffee, brand } from '@/constants/Profile';
+
 const ProfileDetail = () => {
-  return <div>ProfileDetail</div>;
+  return (
+    <>
+      <UserTitle>{userName.loginName}</UserTitle>
+      <Info>
+        <UserSubTitle>카페인 누적 {addedCoffee.cafein}mg</UserSubTitle>
+        <UserBrand>{brand.brand1}</UserBrand>
+      </Info>
+    </>
+  );
 };
 
 export default ProfileDetail;
+
+const UserTitle = styled.span`
+  width: 200px;
+  color: var(--colors-main);
+  font-weight: 700;
+  display: flex;
+  margin-top: 22px;
+`;
+
+const Info = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+const UserSubTitle = styled.span`
+  width: 200px;
+  display: flex;
+`;
+const UserBrand = styled.span`
+  width: 100px;
+  display: flex;
+  justify-content: flex-end;
+  color: var(--colors-sub);
+  font-weight: 700;
+`;
