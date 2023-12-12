243308334c622d83e55dea21a4ca046e3bc3b654
swon95 <shimtkddnjs13@gmail.com>
2023-12-12 13:27:15 +0900

    Feat: Profile Grid

--- src/components/profile/PostsGrid.tsx
+++ src/components/profile/PostsGrid.tsx
@@ -1,5 +1,33 @@
+import { GRID } from '@/constants/PostsGrid';
+import { styled } from 'styled-system/jsx';
+import { Full, Flex, Justify, Grid, Center } from '@/styles/layout';
+import { cx } from 'styled-system/css';
+
 const PostsGrid = () => {
-  return <div>PostsGrid</div>;
+  return (
+    <div className={cx(Full, Justify)}>
+      <div className={cx(Grid, Center)}>
+        {Array.from({ length: GRID.numberOfItems }).map((_, index) => (
+          <div
+            className={Flex}
+            key={index}>
+            {Array.from({ length: GRID.columns }).map((__, colIndex) => (
+              <Container key={colIndex} />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PostsGrid;
+
+const Container = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 15px;
+  padding: 10px;
+  background-color: #ddd;
+  cursor: pointer;
+`;
