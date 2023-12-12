3e8595fbd6af391df96f0d65b364c7602ce1983b
swon95 <shimtkddnjs13@gmail.com>
2023-12-12 14:05:45 +0900

    refactor: Panda css modify

--- src/components/profile/ProfileDetail.tsx
+++ src/components/profile/ProfileDetail.tsx
@@ -1,13 +1,16 @@
 import { styled } from 'styled-system/jsx';
 import { userName, addedCoffee, brand } from '@/constants/Profile';
+import { Flex } from '@/styles/layout';
 
 const ProfileDetail = () => {
   return (
     <>
-      <UserTitle>{userName.loginName}</UserTitle>
-      <Info>
-        <UserSubTitle>카페인 누적 {addedCoffee.cafein}mg</UserSubTitle>
-        <UserBrand>{brand.brand1}</UserBrand>
+      <UserTitle className={Flex}>{userName.loginName}</UserTitle>
+      <Info className={Flex}>
+        <UserSubTitle className={Flex}>
+          카페인 누적 {addedCoffee.cafein}mg
+        </UserSubTitle>
+        <UserBrand className={Flex}>{brand.brand1}</UserBrand>
       </Info>
     </>
   );
@@ -19,23 +22,19 @@ const UserTitle = styled.span`
   width: 200px;
   color: var(--colors-main);
   font-weight: 700;
-  display: flex;
   margin-top: 22px;
 `;
 
 const Info = styled.div`
-  display: flex;
   justify-content: space-between;
   flex-direction: row;
 `;
 
 const UserSubTitle = styled.span`
   width: 200px;
-  display: flex;
 `;
 const UserBrand = styled.span`
   width: 100px;
-  display: flex;
   justify-content: flex-end;
   color: var(--colors-sub);
   font-weight: 700;
