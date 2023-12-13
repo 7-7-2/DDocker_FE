b56a04e53f57433b4b2454a18f8b47bf02e19539
Siwoo Lee <siwoo28@kakao.com>
2023-12-13 15:39:47 +0900

    Feat: input component type,constant 추가

---
+++ src/constants/common.ts
@@ -0,0 +1,26 @@
+export const HEADER_TEXTS = {
+  start: '기본정보'
+};
+
+export const INPUT_TEXTS = {
+  type: {
+    nickname: {
+      typeName: 'nickname',
+      btnText: '중복확인',
+      placeholder: '닉네임을 입력하세요.'
+    },
+    comment: {
+      typeName: 'comment',
+      placeholder: '댓글을 작성해보세요.'
+    },
+    title: { typeName: 'title', placeholder: '제목을 입력하세요.' },
+    search: { typeName: 'search', placeholder: '검색어를 입력해주세요.' }
+  }
+};
+
+export const ALRETMESSAGE_TEXTS = {
+  nickname: {
+    label: '닉네임',
+    message: '사용가능한 닉네임 입니다'
+  }
+};
