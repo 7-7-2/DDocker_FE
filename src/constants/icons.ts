ab96033c36484b34237dfaf51883e2c73379d179
Siwoo Lee <siwoo28@kakao.com>
2023-12-13 15:35:47 +0900

    Feat: input comment icon 추가

---
+++ src/constants/icons.ts
@@ -0,0 +1,16 @@
+export const ICON_SEARCH = {
+  id: 'icon-search',
+  size: '24'
+};
+export const ICON_NOTIFICATION = {
+  id: 'icon-notification',
+  size: '24'
+};
+export const ICON_CLOSE = {
+  id: 'icon-close',
+  size: '24'
+};
+export const ICON_UPLOAD = {
+  id: 'icon-upload',
+  size: '24'
+};
