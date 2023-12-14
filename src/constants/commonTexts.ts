211b8a793cc6bb5bb2d27b84b77bda097b96cc26
swon95 <shimtkddnjs13@gmail.com>
2023-12-14 16:07:54 +0900

    Style: Common Text Apply

---
+++ src/constants/commonTexts.ts
@@ -0,0 +1,9 @@
+export const Text = {
+    ExitButtonText: '회원 탈퇴',
+    SaveButton: '저장하기',
+    Addedcaffeine: '누적 카페인',
+    MG_LABEL: 'mg',
+    DeleteBtn: '삭제',
+    ToggleFollowedBtn: '팔로워',
+    toggleFollowingBtn: '팔로잉,'
+}
