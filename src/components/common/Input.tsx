19c79e89b37570aa2b760c3014c3daf8eec4cd78
Siwoo Lee <siwoo28@kakao.com>
2023-12-14 10:03:46 +0900

    Design: Input Style

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -3,6 +3,10 @@ import Icon from '@/components/common/Icon';
 import { INPUT_TEXTS } from '@/constants/common';
 import { ICON_UPLOAD } from '@/constants/icons';
 import { InputProps } from '@/types/types';
+import { styled } from 'styled-system/jsx';
+import { Align, Between } from '@/styles/layout';
+import { css, cx } from 'styled-system/css';
+import { InputFontSm, InputFontBase } from '@/styles/styles';
 
 export const Input = ({
   type,
@@ -27,7 +31,7 @@ export const Input = ({
   switch (type) {
     case nickname.typeName:
       submitBtn = nickname.btnText;
-      inputLength = `${inputValue?.length}/8`;
+      inputLength = 8;
       inputPlaceholder = nickname.placeholder;
       break;
     case comment.typeName:
@@ -35,7 +39,7 @@ export const Input = ({
       inputPlaceholder = comment.placeholder;
       break;
     case title.typeName:
-      inputLength = `${inputValue?.length}/16`;
+      inputLength = 16;
       inputPlaceholder = title.placeholder;
       break;
     case search.typeName:
@@ -44,24 +48,85 @@ export const Input = ({
   }
 
   return (
-    <div>
-      <div>
-        <input
-          type="text"
-          placeholder={inputPlaceholder}
-          value={inputValue}
-          onChange={handleInputBox}
-        />
-        <div>{inputLength}</div>
+    <InputContainer
+      tabIndex={0}
+      className={
+        type === title.typeName
+          ? cx(Align, Between, TitleInput)
+          : cx(Align, Between)
+      }>
+      <input
+        className={InputFontBase}
+        type="text"
+        placeholder={inputPlaceholder}
+        value={inputValue}
+        onChange={handleInputBox}
+        maxLength={inputLength && inputLength - 1}
+      />
+      <InputBox className={Align}>
+        {inputLength && (
+          <InpoutLength className={Align}>
+            {inputValue?.length}
+            <LengthLimit>/{inputLength}</LengthLimit>
+          </InpoutLength>
+        )}
         {btn && (
           <button
+            className={
+              type === nickname.typeName
+                ? cx(nicknameBtn, InputFontSm)
+                : InputFontSm
+            }
             type="button"
             onClick={handleEvent}
             onTouchEnd={handleEvent}>
             {submitBtn}
           </button>
         )}
-      </div>
-    </div>
+      </InputBox>
+    </InputContainer>
   );
 };
+
+const InputContainer = styled.div`
+  width: 100%;
+  border-radius: 10px;
+  padding: 14px 16px;
+  height: 50px;
+  border: 1px solid #ccc;
+  &:focus-within {
+    border: 1px solid var(--colors-main);
+  }
+`;
+
+const InputBox = styled.div`
+  height: 50px;
+  gap: 8px;
+`;
+
+const TitleInput = css`
+  background-color: #f5f5f5;
+`;
+
+const InpoutLength = styled.div`
+  color: #313131;
+  font-family: Pretendard;
+  font-size: var(--font-sizes-xs);
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+`;
+
+const LengthLimit = styled.div`
+  color: #a6a6a6;
+`;
+
+const nicknameBtn = css`
+  width: 75px;
+  height: 34px;
+  border-radius: 5px;
+  padding: 0 13px;
+  margin-right: -8px;
+  color: #fff;
+  background-color: var(--colors-main);
+`;
