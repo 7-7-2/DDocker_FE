b7f374ed25c5ada8eeb07d9c6c33856d08c6cffa
Siwoo Lee <siwoo28@kakao.com>
2023-12-14 11:28:10 +0900

    Feat: Nickname Label Icon 추가

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,16 +1,19 @@
 const Icon = ({
   id,
   size,
+  fill,
   onTouchEnd
 }: {
   id: string;
   size: string;
+  fill?: string;
   onTouchEnd?: () => void;
 }) => {
   return (
     <svg
       width={size}
       height={size}
+      fill={fill}
       onTouchEnd={onTouchEnd}>
       <use href={`/sprite.svg#${id}`} />
     </svg>
