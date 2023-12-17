18c630d9c35b203eafa6d058e33c09ef40c66a82
Siwoo Lee <siwoo28@kakao.com>
2023-12-17 18:08:34 +0900

    Fix: icon 수정 및 추가, 사소한 수정

--- src/constants/start.ts
+++ src/constants/start.ts
@@ -7,12 +7,99 @@ export const SIGININ_TEXTS = {
     google: '구글 로그인',
     kakao: '카카오 로그인',
     none: '로그인 없이 둘러보기'
-  },
-  initialForm: {
-    message: {
-      first: '환영합니다!',
-      second: '을 등록해 보세요!',
-      profile: '프로필'
-    }
   }
 };
+
+export const INITIAL_FORM_TEXTS = {
+  message: {
+    first: '환영합니다!',
+    second: '을 등록해 보세요!',
+    profile: '프로필'
+  },
+  gender: {
+    male: '남성',
+    female: '여성'
+  }
+};
+
+export const SELECTFAVBRAND_TEXTS = {
+  message: {
+    first: '선호 브랜드를 선택해주세요',
+    second: '선호하는 커피 브랜드를 한가지만 선택해주세요.'
+  }
+};
+
+export const BRANDLIST = [
+  {
+    brand: '바나프레소',
+    icon: 'Img'
+  },
+  {
+    brand: '스타벅스',
+    icon: 'Img'
+  },
+  {
+    brand: '이디야',
+    icon: 'Img'
+  },
+  {
+    brand: '메가커피',
+    icon: 'Img'
+  },
+  {
+    brand: '할리스',
+    icon: 'Img'
+  },
+  {
+    brand: '정월',
+    icon: 'Img'
+  },
+  {
+    brand: '탐탐',
+    icon: 'Img'
+  },
+  {
+    brand: '카페베네',
+    icon: 'Img'
+  },
+  {
+    brand: '엉클커피',
+    icon: 'Img'
+  },
+  {
+    brand: '탐탐',
+    icon: 'Img'
+  },
+  {
+    brand: '카페베네',
+    icon: 'Img'
+  },
+  {
+    brand: '엉클커피',
+    icon: 'Img'
+  },
+  {
+    brand: '탐탐',
+    icon: 'Img'
+  },
+  {
+    brand: '카페베네',
+    icon: 'Img'
+  },
+  {
+    brand: '엉클커피',
+    icon: 'Img'
+  },
+  {
+    brand: '탐탐',
+    icon: 'Img'
+  },
+  {
+    brand: '카페베네',
+    icon: 'Img'
+  },
+  {
+    brand: '엉클커피',
+    icon: 'Img'
+  }
+];
