ef27fae9065ede09379be6514cf19ee5255c859c
Siwoo Lee <siwoo28@kakao.com>
2023-12-17 18:09:08 +0900

    Design: Button stylec 추가

--- src/pages/Page-Home.tsx
+++ src/pages/Page-Home.tsx
@@ -1,9 +1,72 @@
+import { authState } from '@/atoms/atoms';
+import Button from '@/components/common/Button';
 import { useComposeHeader } from '@/hooks/useComposeHeader';
+import { Grid } from '@/styles/layout';
+import {
+  Btn,
+  DefaultBtn,
+  FollowBtn,
+  FollowBtnSm,
+  FollowingBtn,
+  FollowingBtnSm,
+  LoginBtn,
+  RegistBtn
+} from '@/styles/styles';
+import { useRecoilValue } from 'recoil';
+import { styled } from 'styled-system/jsx';
 
 const Home = () => {
   useComposeHeader(true, '', 'icons');
-
-  return <>HOME</>;
+  const { user } = useRecoilValue(authState);
+  console.log(user);
+  const test = () => {
+    console.log(Btn);
+  };
+  return (
+    <>
+      <Gap className={Grid}>
+        <Button
+          text="다음으로"
+          onTouchEnd={test}
+          className={DefaultBtn}
+        />
+        <Button
+          text="등록하기"
+          onTouchEnd={test}
+          className={RegistBtn}
+        />
+        <Button
+          text="로그인"
+          onTouchEnd={test}
+          className={LoginBtn}
+        />
+        <Button
+          text="팔로우"
+          onTouchEnd={test}
+          className={FollowBtn}
+        />
+        <Button
+          text="팔로잉"
+          onTouchEnd={test}
+          className={FollowingBtn}
+        />
+        <Button
+          text="팔로우"
+          onTouchEnd={test}
+          className={FollowBtnSm}
+        />
+        <Button
+          text="팔로잉"
+          onTouchEnd={test}
+          className={FollowingBtnSm}
+        />
+      </Gap>
+    </>
+  );
 };
 
 export default Home;
+const Gap = styled.div`
+  grid-template-columns: 1fr;
+  gap: 20px 0;
+`;
