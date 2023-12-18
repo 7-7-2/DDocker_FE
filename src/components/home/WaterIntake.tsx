98cc9da8d3dad511c278e8a03a16801033d4f74d
Siwoo Lee <siwoo28@kakao.com>
2023-12-18 13:52:44 +0900

    Design: WaterPerCoffee component

--- src/components/home/WaterIntake.tsx
+++ src/components/home/WaterIntake.tsx
@@ -1,7 +1,44 @@
+import Icon from '@/components/common/Icon';
+import { Align, Justify } from '@/styles/layout';
+import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
+import { css, cx } from 'styled-system/css';
+import { styled } from 'styled-system/jsx';
+
 const WaterIntake = () => {
   return (
-    <div>WaterIntake</div>
-  )
-}
+    <Container className={cx(Align, Justify)}>
+      <div className={cx(progress, Justify, Align)}>
+        <img
+          src="/waterCup.png"
+          alt="water"
+        />
+        <IconContainer>
+          <Icon {...iconPropsGenerator('plus')} />
+        </IconContainer>
+      </div>
+    </Container>
+  );
+};
 
-export default WaterIntake
+export default WaterIntake;
+
+const Container = styled.div`
+  position: relative;
+  width: 106px;
+  height: 106px;
+  border-radius: 50%;
+  background-color: aliceblue;
+`;
+
+const progress = css`
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  background-color: #ffffff;
+`;
+
+const IconContainer = styled.div`
+  position: absolute;
+  right: 30px;
+  bottom: 25px;
+`;
