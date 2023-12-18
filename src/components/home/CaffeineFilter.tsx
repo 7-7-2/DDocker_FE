3f7299db01219430922f3847a18306ba02a0a33c
Siwoo Lee <siwoo28@kakao.com>
2023-12-18 17:58:32 +0900

    Feat: CaffeineFilter component

--- src/components/home/CaffeineFilter.tsx
+++ src/components/home/CaffeineFilter.tsx
@@ -1,5 +1,23 @@
+import CaffeineInfo from '@/components/common/CaffeineInfo';
+import Button from '@/components/common/Button';
+import CoffeeSelection from '@/components/home/CoffeeSelection';
+import { BUTTON_TEXTS } from '@/constants/common';
+import { RegistBtn } from '@/styles/styles';
+
 const CaffeineFilter = () => {
-  return <div>CaffeineFilter</div>;
+  return (
+    <div>
+      <CoffeeSelection />
+      <CaffeineInfo />
+      <Button
+        text={BUTTON_TEXTS.regist}
+        onTouchEnd={() => {
+          console.log('hi');
+        }}
+        className={RegistBtn}
+      />
+    </div>
+  );
 };
 
 export default CaffeineFilter;
