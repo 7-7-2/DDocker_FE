4200f2bb9274b73f3a069fea7a2299a1cb7e1f55
hookor <gerrard1219@gmail.com>
2023-12-18 17:30:55 +0900

    Feat: posts

--- src/pages/Page-Posts.tsx
+++ src/pages/Page-Posts.tsx
@@ -1,9 +1,34 @@
 import { useComposeHeader } from '@/hooks/useComposeHeader';
+import PostCard from '@/components/posts/PostCard';
+import { styled } from 'styled-system/jsx';
 
 export const Posts = () => {
   useComposeHeader(true, '', 'icons');
 
-  return <>POSTS</>;
+  // DATA.MAP => <PostCard />
+  return (
+    <Container>
+      <PostCard
+        userId="zxc"
+        NickName="커피안마셔안마셔"
+        caffeine={12345}
+      />
+      <PostCard
+        userId="zxc"
+        NickName="커피안마셔안마셔"
+        caffeine={12345}
+      />
+      <PostCard
+        userId="zxc"
+        NickName="커피안마셔안마셔"
+        caffeine={12345}
+      />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  padding: 20px 0;
+`;
 
 export default Posts;
