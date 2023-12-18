bac8d19d61c9aff819dc5efb999a41f9f0808a44
swon95 <shimtkddnjs13@gmail.com>
2023-12-18 16:19:36 +0900

    Feat: Search ListItem Component

--- src/pages/Page-Search.tsx
+++ src/pages/Page-Search.tsx
@@ -1,10 +1,16 @@
 import SearchBar from '@/components/search/SearchBar';
 import SearchListItem from '@/components/search/SearchListItem';
+import { SimplifyUser } from '@/types/types';
+
 const Search = () => {
+  const usersData: SimplifyUser[] = [
+    // { userId: '1', NickName: '내가바로커피왕', caffeine: 34054 },
+    // { userId: '2', NickName: '커피커피마셔', caffeine: 2560 }
+  ];
   return (
     <>
       <SearchBar />
-      <SearchListItem />
+      {usersData.length > 0 ? <SearchListItem users={usersData} /> : <></>}
     </>
   );
 };
