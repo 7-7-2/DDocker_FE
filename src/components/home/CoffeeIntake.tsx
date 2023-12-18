3fa49365d893a71a846abe60c2687ed0d5c4a41e
Siwoo Lee <siwoo28@kakao.com>
2023-12-18 10:09:48 +0900

    Feat: CoffeeIntake component

--- src/components/home/CoffeeIntake.tsx
+++ src/components/home/CoffeeIntake.tsx
@@ -1,7 +1,33 @@
-const CoffeeIntake = () => {
-  return (
-    <div>CoffeeIntake</div>
-  )
-}
+import { testData } from '@/types/types';
+import { styled } from 'styled-system/jsx';
 
-export default CoffeeIntake
+const CoffeeIntake = ({ data }: { data: testData }) => {
+  return (
+    <div>
+      <div>오늘의 카페인 섭취량</div>
+      <div>
+        {data.Allcaffeine}mg/{data.coffee}잔
+      </div>
+      <div>일일 섭취 권장량</div>
+      <div>400mg</div>
+      <ProgressBar>
+        <Progress></Progress>
+      </ProgressBar>
+    </div>
+  );
+};
+
+export default CoffeeIntake;
+
+const ProgressBar = styled.div`
+  position: relative;
+  width: 100px;
+  height: 10px;
+  background-color: aliceblue;
+`;
+const Progress = styled.div`
+  position: absolute;
+  width: 40%;
+  height: 10px;
+  background-color: #152d43;
+`;
