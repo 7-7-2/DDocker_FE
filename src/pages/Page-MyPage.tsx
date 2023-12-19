413c6b9e3ce3452c4309d0090a2dbad804a1432d
swon95 <shimtkddnjs13@gmail.com>
2023-12-19 17:34:16 +0900

    Refactor: Header area Apply

--- src/pages/Page-MyPage.tsx
+++ src/pages/Page-MyPage.tsx
@@ -1,8 +1,6 @@
 import MyProfile from '@/components/mypage/MyProfile';
-import { useComposeHeader } from '@/hooks/useComposeHeader';
 
 export const MyPage = () => {
-  useComposeHeader(true, '', 'icons');
   return (
     <>
       <MyProfile />
