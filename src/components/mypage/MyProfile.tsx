327d5cbcd87406d9a99ea284fa512a9dc039e000
swon95 <shimtkddnjs13@gmail.com>
2023-12-21 03:11:41 +0900

    Feat: Image Regist

--- src/components/mypage/MyProfile.tsx
+++ src/components/mypage/MyProfile.tsx
@@ -1,3 +1,6 @@
+import { useRef } from 'react';
+import { useRecoilState } from 'recoil';
+import { imageState } from '@/atoms/atoms';
 import Icon from '@/components/common/Icon';
 import CheckNickname from '@/components/start/CheckNickname';
 import { TEXT } from '@/constants/texts';
@@ -16,16 +19,50 @@ const MyProfile = () => {
   const handleExitedUser = () => {
     console.log('회원 탈퇴');
   };
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [profileImage, setProfileImage] = useRecoilState(imageState);
+
+  const handleLoadImage = (e: any) => {
+    if (!e.target.files?.length) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onloadend = () => {
+      const base64 = reader.result;
+      if (base64) {
+        setProfileImage(base64?.toString());
+      }
+    };
+  };
+
   return (
     <>
       <Wrapper className={cx(FlexCenter, Column)}>
         <Container className={cx(Flex, MarginAuto)}>
           <Box className={cx(FlexCenter, MarginAuto)}>
             <div>
-              <Icon {...iconPropsGenerator('user', '100')} />
+              {profileImage ? (
+                <ImgContainer
+                  src={profileImage}
+                  alt="Profile"
+                />
+              ) : (
+                <Icon {...iconPropsGenerator('user', '100')} />
+              )}
             </div>
             <Edit className={Cursor}>
-              <Icon {...iconPropsGenerator('edit-photo', '32')} />
+              <label>
+                <Icon {...iconPropsGenerator('edit-photo', '32')} />
+                <InputBtn
+                  type="file"
+                  accept="image/*"
+                  ref={fileInputRef}
+                  onChange={handleLoadImage}
+                />
+              </label>
             </Edit>
           </Box>
         </Container>
@@ -55,21 +92,29 @@ const Wrapper = styled.div`
 `;
 const Container = styled.div`
   position: relative;
-  width: 94px;
-  height: 94px;
+  width: 100px;
+  height: 100px;
 `;
 const Box = styled.div`
-  width: 54px;
-  height: 54px;
+  width: 100px;
+  height: 100px;
   position: relative;
+`;
+const ImgContainer = styled.img`
+  width: auto;
+  height: auto;
+  border-radius: 50%;
 `;
 const Edit = styled.div`
   position: absolute;
   width: 30px;
   height: 30px;
-  bottom: -15px;
-  right: -25px;
+  bottom: 5px;
+  right: 0px;
   z-index: 1;
+`;
+const InputBtn = styled.input`
+  display: none;
 `;
 const ExitButton = styled.span`
   font-size: var(--font--sizes-sm);
