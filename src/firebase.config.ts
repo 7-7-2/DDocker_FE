0337e60304d0756d9ed0af83f8918b972038d25b
swon95 <shimtkddnjs13@gmail.com>
2023-12-21 20:39:55 +0900

    Feat: Firebase Storage

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,5 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, firestore };
+export { app, firestore, storage };
