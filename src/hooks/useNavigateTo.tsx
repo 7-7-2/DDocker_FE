20ad2e88431c7bb3418fe41013ba0e564e52ea7c
hookor <gerrard1219@gmail.com>
2023-12-22 06:15:43 +0900

    Fix: useNavigateTo back

--- src/hooks/useNavigateTo.tsx
+++ src/hooks/useNavigateTo.tsx
@@ -3,6 +3,10 @@ import { useNavigate } from 'react-router-dom';
 export const useNavigateTo = (to: string) => {
   const navigate = useNavigate();
   const toWhere = () => {
+    if (to === '-1') {
+      navigate(-1);
+      return;
+    }
     navigate(to);
   };
 
