80747e38c9752b5eca4205a6968c828a417a52e9
Siwoo Lee <siwoo28@kakao.com>
2023-12-22 15:54:55 +0900

    Docs: coffeeData update

---
+++ src/datas/coffees.ts
@@ -0,0 +1,941 @@
+const coffeeData = {
+  엔젤리너스: [
+    {
+      brand: '엔제리너스',
+      name: '돌체라떼',
+      caffeine: '317'
+    },
+    {
+      brand: '엔제리너스',
+      name: '카페 연유다',
+      caffeine: '176'
+    },
+    {
+      brand: '엔제리너스',
+      name: '아메리치노 라떼',
+      caffeine: '317'
+    },
+    {
+      brand: '엔제리너스',
+      name: '아메리치노(Original)',
+      caffeine: '317'
+    },
+    {
+      brand: '엔제리너스',
+      name: '콜드브루 커피',
+      caffeine: '498'
+    },
+    {
+      brand: '엔제리너스',
+      name: '아메리카노',
+      caffeine: '317'
+    },
+    {
+      brand: '엔제리너스',
+      name: '카푸치노',
+      caffeine: '211'
+    },
+    {
+      brand: '엔제리너스',
+      name: '카페라떼',
+      caffeine: '211'
+    },
+    {
+      brand: '엔제리너스',
+      name: '바닐라 카페라떼',
+      caffeine: '211'
+    },
+    {
+      brand: '엔제리너스',
+      name: '카라멜마끼아또',
+      caffeine: '211'
+    },
+    {
+      brand: '엔제리너스',
+      name: '카페모카',
+      caffeine: '221'
+    }
+  ],
+  빽다방: [
+    {
+      brand: '빽다방',
+      name: '더블에스프레소',
+      caffeine: '237'
+    },
+    {
+      brand: '빽다방',
+      name: '앗!메리카노',
+      caffeine: '237'
+    },
+    {
+      brand: '빽다방',
+      name: '원조커피',
+      caffeine: '390'
+    },
+    {
+      brand: '빽다방',
+      name: '달달연유라떼',
+      caffeine: '237'
+    },
+    {
+      brand: '빽다방',
+      name: "빽's 카페라떼",
+      caffeine: '237'
+    },
+    {
+      brand: '빽다방',
+      name: '바닐라라떼',
+      caffeine: '237'
+    },
+    {
+      brand: '빽다방',
+      name: '카페모카',
+      caffeine: '237'
+    },
+    {
+      brand: '빽다방',
+      name: '카라멜마키아또',
+      caffeine: '237'
+    },
+    {
+      brand: '빽다방',
+      name: '코코넛카페라떼',
+      caffeine: '237'
+    },
+    {
+      brand: '빽다방',
+      name: '콜드브루',
+      caffeine: '195'
+    },
+    {
+      brand: '빽다방',
+      name: '콜드브루라떼',
+      caffeine: '195'
+    },
+    {
+      brand: '빽다방',
+      name: '콜드브루라떼(연유)',
+      caffeine: '195'
+    },
+    {
+      brand: '빽다방',
+      name: '콜드브루라떼(흑당)',
+      caffeine: '195'
+    }
+  ],
+  컴포즈커피: [
+    {
+      brand: '컴포즈커피',
+      name: '아메리카노',
+      caffeine: '156'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '카페라떼',
+      caffeine: '208'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '바닐라라떼',
+      caffeine: '208'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '헤이즐넛라떼',
+      caffeine: '208'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '카페모카',
+      caffeine: '208'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '카라멜마끼아또',
+      caffeine: '208'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '돌체라떼',
+      caffeine: '208'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '더치커피',
+      caffeine: '198'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '더치라떼',
+      caffeine: '198'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '아인슈페너',
+      caffeine: '198'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '아인슈페너 라떼',
+      caffeine: '208'
+    },
+    {
+      brand: '컴포즈커피',
+      name: '달고나 라떼',
+      caffeine: '156'
+    }
+  ],
+  이디야: [
+    {
+      brand: '이디야',
+      name: '바닐라 라떼',
+      caffeine: '103'
+    },
+    {
+      brand: '이디야',
+      name: '버블 흑당 콜드브루',
+      caffeine: '65'
+    },
+    {
+      brand: '이디야',
+      name: '아인슈페너',
+      caffeine: '103'
+    },
+    {
+      brand: '이디야',
+      name: '연유 카페라떼',
+      caffeine: '91'
+    },
+    {
+      brand: '이디야',
+      name: '연유 콜드브루',
+      caffeine: '130'
+    },
+    {
+      brand: '이디야',
+      name: '카라멜 마끼아또',
+      caffeine: '91'
+    },
+    {
+      brand: '이디야',
+      name: '카라멜 플랫치노',
+      caffeine: '91'
+    },
+    {
+      brand: '이디야',
+      name: '카페 라떼',
+      caffeine: '91'
+    },
+    {
+      brand: '이디야',
+      name: '카페 모카',
+      caffeine: '99'
+    },
+    {
+      brand: '이디야',
+      name: '카페 아메리카노',
+      caffeine: '91'
+    },
+    {
+      brand: '이디야',
+      name: '카푸치노',
+      caffeine: '91'
+    },
+    {
+      brand: '이디야',
+      name: '커피 플랫치노',
+      caffeine: '91'
+    },
+    {
+      brand: '이디야',
+      name: '콜드브루 니트로',
+      caffeine: '155'
+    },
+    {
+      brand: '이디야',
+      name: '콜드브루 라떼',
+      caffeine: '130'
+    },
+    {
+      brand: '이디야',
+      name: '콜드브루 아메리카노',
+      caffeine: '130'
+    },
+    {
+      brand: '이디야',
+      name: '콜드브루 크림넛',
+      caffeine: '130'
+    },
+    {
+      brand: '이디야',
+      name: '콜드브루 화이트 비엔나',
+      caffeine: '65'
+    },
+    {
+      brand: '이디야',
+      name: '크림샷 라떼',
+      caffeine: '93'
+    }
+  ],
+  할리스: [
+    {
+      brand: '할리스',
+      name: '프리미엄 블렌드 아메리카노',
+      caffeine: '233'
+    },
+    {
+      brand: '할리스',
+      name: '콜드브루 딜라이트',
+      caffeine: '148'
+    },
+    {
+      brand: '할리스',
+      name: '프리미엄 블렌드 딥 라떼',
+      caffeine: '111'
+    },
+    {
+      brand: '할리스',
+      name: '바닐라 딜라이트',
+      caffeine: '80'
+    },
+    {
+      brand: '할리스',
+      name: '리스트레또 딜라이트',
+      caffeine: '93'
+    },
+    {
+      brand: '할리스',
+      name: '콜드브루 라떼',
+      caffeine: '148'
+    },
+    {
+      brand: '할리스',
+      name: '콜드브루',
+      caffeine: '137'
+    },
+    {
+      brand: '할리스',
+      name: '카라멜 마키아또',
+      caffeine: '127'
+    },
+    {
+      brand: '할리스',
+      name: '카페모카',
+      caffeine: '132'
+    },
+    {
+      brand: '할리스',
+      name: '카페라떼',
+      caffeine: '127'
+    },
+    {
+      brand: '할리스',
+      name: '아메리카노',
+      caffeine: '114'
+    },
+    {
+      brand: '할리스',
+      name: '아포가토',
+      caffeine: '93'
+    }
+  ],
+  메가커피: [
+    {
+      brand: '메가커피',
+      name: '아메리카노',
+      caffeine: '204'
+    },
+    {
+      brand: '메가커피',
+      name: '티라미수 라떼',
+      caffeine: '206'
+    },
+    {
+      brand: '메가커피',
+      name: '꿀아메리카노',
+      caffeine: '199'
+    },
+    {
+      brand: '메가커피',
+      name: '바닐라 라떼',
+      caffeine: '189'
+    },
+    {
+      brand: '메가커피',
+      name: '바닐라 아메리카노',
+      caffeine: '196'
+    },
+    {
+      brand: '메가커피',
+      name: '연유 라떼',
+      caffeine: '194'
+    },
+    {
+      brand: '메가커피',
+      name: '카라멜 마끼아또',
+      caffeine: '206'
+    },
+    {
+      brand: '메가커피',
+      name: '카페라떼',
+      caffeine: '189'
+    },
+    {
+      brand: '메가커피',
+      name: '카페모카',
+      caffeine: '211'
+    },
+    {
+      brand: '메가커피',
+      name: '카푸치노',
+      caffeine: '201'
+    },
+    {
+      brand: '메가커피',
+      name: '콜드브루 라떼',
+      caffeine: '216'
+    },
+    {
+      brand: '메가커피',
+      name: '콜드브루 오리지널',
+      caffeine: '217'
+    },
+    {
+      brand: '메가커피',
+      name: '헤이즐넛 라떼',
+      caffeine: '204'
+    },
+    {
+      brand: '메가커피',
+      name: '헤이즐넛아메리카노',
+      caffeine: '209'
+    },
+
+    {
+      brand: '메가커피',
+      name: '메가리카노',
+      caffeine: '290'
+    },
+
+    {
+      brand: '메가커피',
+      name: '큐브라떼',
+      caffeine: '313'
+    }
+  ],
+  파스쿠찌: [
+    {
+      brand: '파스쿠찌',
+      name: '헤이오트라떼',
+      caffeine: '227'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '아메리카노',
+      caffeine: '227'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '카페라떼',
+      caffeine: '188'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '카푸치노',
+      caffeine: '190'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '카라멜 라떼 마끼아또',
+      caffeine: '196'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '바닐라 라떼 마끼아또',
+      caffeine: '188'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '카페모카',
+      caffeine: '192'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '시그니처 크림 커피',
+      caffeine: '239'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '콜드브루 아메리카노',
+      caffeine: '201'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '콜드브루 라떼',
+      caffeine: '190'
+    },
+    {
+      brand: '파스쿠찌',
+      name: '연유 콜드브루 플로트',
+      caffeine: '160'
+    }
+  ],
+  폴바셋: [
+    {
+      brand: '폴바셋',
+      name: '룽고',
+      caffeine: '185'
+    },
+    {
+      brand: '폴바셋',
+      name: '아메리카노',
+      caffeine: '160'
+    },
+    {
+      brand: '폴바셋',
+      name: '리스트레토',
+      caffeine: '150'
+    },
+    {
+      brand: '폴바셋',
+      name: '에스프레소',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '마키아토',
+      caffeine: '113'
+    },
+    {
+      brand: '폴바셋',
+      name: '허니 에스프레소 콘 파나',
+      caffeine: '185'
+    },
+    {
+      brand: '폴바셋',
+      name: '초콜릿 에스프레소 콘 파나',
+      caffeine: '186'
+    },
+    {
+      brand: '폴바셋',
+      name: '시나몬 에스프레소 콘 파나',
+      caffeine: '185'
+    },
+    {
+      brand: '폴바셋',
+      name: '플랫 화이트',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '카페라떼',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '아이스크림 카페 라떼',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '카푸치노',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '아이스 스페니쉬 카페 라떼[연유]',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '스페니쉬 카페 라떼[연유]',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '아이스 바닐라 카페 라떼',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '바닐라 카페 라떼',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '아이스 카라멜 마키아토',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '아이스 시나몬 카페라떼',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '시나몬 카페라떼',
+      caffeine: '225'
+    },
+    {
+      brand: '폴바셋',
+      name: '흑임자 카페라떼',
+      caffeine: '185'
+    },
+    {
+      brand: '폴바셋',
+      name: '콜드브루',
+      caffeine: '165'
+    },
+    {
+      brand: '폴바셋',
+      name: '콜드브루 라떼',
+      caffeine: '165'
+    },
+    {
+      brand: '폴바셋',
+      name: '스페니쉬 크림 콜드브루',
+      caffeine: '200'
+    },
+    {
+      brand: '폴바셋',
+      name: '스페셜티 커피',
+      caffeine: '170'
+    }
+  ],
+  스타벅스: [
+    {
+      brand: '스타벅스',
+      name: '나이트로 바닐라 크림',
+      caffeine: '232'
+    },
+    {
+      brand: '스타벅스',
+      name: '나이트로 콜드 브루',
+      caffeine: '245'
+    },
+    {
+      brand: '스타벅스',
+      name: '돌체 콜드 브루',
+      caffeine: '150'
+    },
+    {
+      brand: '스타벅스',
+      name: '바닐라 크림 콜드 브루',
+      caffeine: '150'
+    },
+    {
+      brand: '스타벅스',
+      name: '벨벳 다크 모카 나이트로',
+      caffeine: '190'
+    },
+    {
+      brand: '스타벅스',
+      name: '시그니처 더 블랙 콜드 브루',
+      caffeine: '680'
+    },
+    {
+      brand: '스타벅스',
+      name: '제주 비자림 콜드 브루',
+      caffeine: '105'
+    },
+    {
+      brand: '스타벅스',
+      name: '콜드 브루',
+      caffeine: '150'
+    },
+    {
+      brand: '스타벅스',
+      name: '콜드 브루 몰트',
+      caffeine: '190'
+    },
+    {
+      brand: '스타벅스',
+      name: '콜드 브루 오트 라떼',
+      caffeine: '65'
+    },
+    {
+      brand: '스타벅스',
+      name: '콜드 브루 플로트',
+      caffeine: '190'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 커피',
+      caffeine: '140'
+    },
+    {
+      brand: '스타벅스',
+      name: '오늘의 커피',
+      caffeine: '260'
+    },
+    {
+      brand: '스타벅스',
+      name: '에스프레소 콘 파나',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '에스프레소 마키아또',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 카페 아메리카노',
+      caffeine: '150'
+    },
+    {
+      brand: '스타벅스',
+      name: '카페 아메리카노',
+      caffeine: '150'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 카라멜 마키아또',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '카라멜 마키아또',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 카푸치노',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '카푸치노',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '라벤더 카페 브레베',
+      caffeine: '105'
+    },
+    {
+      brand: '스타벅스',
+      name: '럼 샷 코르타도',
+      caffeine: '160'
+    },
+    {
+      brand: '스타벅스',
+      name: '바닐라 빈 라떼',
+      caffeine: '210'
+    },
+    {
+      brand: '스타벅스',
+      name: '사케라또 비안코 오버 아이스',
+      caffeine: '315'
+    },
+    {
+      brand: '스타벅스',
+      name: '스타벅스 돌체라떼',
+      caffeine: '150'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 라벤더 카페 브레베',
+      caffeine: '105'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 바닐라 빈 라떼',
+      caffeine: '210'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 스타벅스 돌체 라떼',
+      caffeine: '150'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 카페 라떼',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '카페 라떼',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 카페 모카',
+      caffeine: '95'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 화이트 초콜릿 모카',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '카페 모카',
+      caffeine: '95'
+    },
+    {
+      brand: '스타벅스',
+      name: '화이트 초콜릿 모카',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '바닐라 플랫 화이트',
+      caffeine: '260'
+    },
+    {
+      brand: '스타벅스',
+      name: '라벤더 블론드 스타벅스 더블 샷',
+      caffeine: '270'
+    },
+    {
+      brand: '스타벅스',
+      name: '바닐라 스타벅스 더블 샷',
+      caffeine: '150'
+    },
+    {
+      brand: '스타벅스',
+      name: '블론드 바닐라 더블 샷 마키아또',
+      caffeine: '170'
+    },
+    {
+      brand: '스타벅스',
+      name: '사케라또 아포가토',
+      caffeine: '210'
+    },
+    {
+      brand: '스타벅스',
+      name: '스파클링 시트러스 에스프레소',
+      caffeine: '105'
+    },
+    {
+      brand: '스타벅스',
+      name: '아이스 블론드 바닐라 더블 샷 마키아또',
+      caffeine: '170'
+    },
+    {
+      brand: '스타벅스',
+      name: '에스프레소',
+      caffeine: '75'
+    },
+    {
+      brand: '스타벅스',
+      name: '커피 스타벅스 더블 샷',
+      caffeine: '150'
+    },
+    {
+      brand: '스타벅스',
+      name: '클래식 아포가토',
+      caffeine: '210'
+    },
+    {
+      brand: '스타벅스',
+      name: '헤이즐넛 스타벅스 더블 샷',
+      caffeine: '150'
+    }
+  ],
+  더벤티: [
+    {
+      brand: '더벤티',
+      name: '아인슈페너',
+      caffeine: '264'
+    },
+    {
+      brand: '더벤티',
+      name: '아메리카노',
+      caffeine: '215'
+    },
+    {
+      brand: '더벤티',
+      name: '바닐라 다크리카노',
+      caffeine: '264'
+    },
+    {
+      brand: '더벤티',
+      name: '헤이즐넛 다크리카노',
+      caffeine: '264'
+    },
+    {
+      brand: '더벤티',
+      name: '꿀 다크리카노',
+      caffeine: '264'
+    },
+    {
+      brand: '더벤티',
+      name: '믹스커피',
+      caffeine: '110'
+    },
+    {
+      brand: '더벤티',
+      name: '카페라떼',
+      caffeine: '264'
+    },
+    {
+      brand: '더벤티',
+      name: '바닐라딥라떼',
+      caffeine: '264'
+    },
+    {
+      brand: '더벤티',
+      name: '헤이즐넛딥라떼',
+      caffeine: '264'
+    },
+    {
+      brand: '더벤티',
+      name: '토피넛라떼',
+      caffeine: '132'
+    },
+    {
+      brand: '더벤티',
+      name: '연유라떼',
+      caffeine: '264'
+    },
+    {
+      brand: '더벤티',
+      name: '카라멜 마끼야또',
+      caffeine: '264'
+    },
+    {
+      brand: '더벤티',
+      name: '카페모카',
+      caffeine: '273'
+    },
+    {
+      brand: '더벤티',
+      name: '콜드브루',
+      caffeine: '92'
+    },
+    {
+      brand: '더벤티',
+      name: '콜드브루 라떼',
+      caffeine: '92'
+    },
+    {
+      brand: '더벤티',
+      name: '바닐라 크림 콜드브루',
+      caffeine: '79'
+    },
+    {
+      brand: '더벤티',
+      name: '헤이즐넛 크림 콜드브루',
+      caffeine: '79'
+    },
+    {
+      brand: '더벤티',
+      name: '더벤티 사이즈 아메리카노',
+      caffeine: '480'
+    },
+    {
+      brand: '더벤티',
+      name: '더벤티 사이즈 카페라떼',
+      caffeine: '528'
+    },
+    {
+      brand: '더벤티',
+      name: '더벤티 사이즈 믹스커피',
+      caffeine: '165'
+    }
+  ]
+};
+
+export default coffeeData;
