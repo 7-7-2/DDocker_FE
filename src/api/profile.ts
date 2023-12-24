185d84996bbdd40b3eab18bd75db1af6a7312848
swon95 <shimtkddnjs13@gmail.com>
2023-12-25 04:00:08 +0900

    Fix: Build Error Modify

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -21,9 +21,11 @@ export const setStorageImg = async (path: string, file: File) => {
 // storage 에 저장된 이미지를 다운로드 하는 함수
 export const getStorageImg = async (path: string): Promise<string | void> => {
   const storageRef = ref(storage, path);
-  return await getDownloadURL(storageRef).catch(error =>
+  try {
+    return await getDownloadURL(storageRef);
+  } catch (error) {
     console.log('다운로드 실패:', error)
-  );
+  };
 };
 
 // 생성된 문서에 다운로드 받은 이미지를 업데이트(수정, 추가) 하는 함수
