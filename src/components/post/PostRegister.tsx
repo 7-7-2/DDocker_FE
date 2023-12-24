28561df7a70315c641016175469b6acc9fe5f3f4
hookor <gerrard1219@gmail.com>
2023-12-24 16:44:19 +0900

    Feat: Register Img Edit

--- src/components/post/PostRegister.tsx
+++ src/components/post/PostRegister.tsx
@@ -1,3 +1,9 @@
+import '@pqina/pintura/pintura.css';
+import { useRef, useState } from 'react';
+import { PinturaEditorModal } from '@pqina/react-pintura';
+import { getEditorDefaults, createDefaultImageWriter } from '@pqina/pintura';
+import locale_ko_KR from '@pqina/pintura/locale/ko_KR';
+// _PINTURA
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { Input } from '@/components/common/Input';
 import CoffeeOptionSelection from '@/components/common/CoffeeOptionSelection';
@@ -10,28 +16,59 @@ import {
   registPostState,
   useInputState
 } from '@/atoms/atoms';
+import { useStorage } from '@/api/profile';
+
 import { useShowFooter } from '@/hooks/useShowFooter';
+import { useNavigateTo } from '@/hooks/useNavigateTo';
+import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
+import Icon from '@/components/common/Icon';
+import Button from '@/components/common/Button';
 
 import { css, cx } from 'styled-system/css';
 import { styled } from 'styled-system/jsx';
-import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
-import Icon from '@/components/common/Icon';
 import { Center, Flex } from '@/styles/layout';
-import Button from '@/components/common/Button';
 import { DefaultBtn } from '@/styles/styles';
-import { useNavigateTo } from '@/hooks/useNavigateTo';
+
+const editorDefaults = getEditorDefaults({
+  cropImageSelectionCornerStyle: 'hook',
+  locale: {
+    ...locale_ko_KR
+  }
+});
 
 const PostRegister = () => {
   useShowFooter(false);
   const inputState = useRecoilValue(useInputState);
   const [registInfo, setRegistInfo] = useRecoilState(registPostState);
 
+  const [editorEnabled, setEditorEnabled] = useState(false);
+  const [editorSrc, setEditorSrc] = useState<File>();
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  const { uploadFile } = useStorage();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleInputChange = () => {
+    if (!fileInputRef?.current?.files?.length) return;
+    if (fileInputRef?.current?.files) {
+      setEditorEnabled(true);
+      setEditorSrc(fileInputRef?.current?.files[0]);
+    }
+  };
+
+  const handleEditorHide = () => setEditorEnabled(false);
+
+  const handleEditorProcess = ({ dest }: { dest: File }) => {
+    const url = URL.createObjectURL(dest);
+    setImageUrl(url);
+  };
+
   const newRegistData = {
     ...registInfo,
     title: inputState
   };
 
-  const navigateToDetail = useNavigateTo('/Post/1');
+  const navigateToDetail = useNavigateTo('/post/1');
 
   const clickRegisterBtn = () => {
     navigateToDetail;
@@ -52,11 +89,66 @@ const PostRegister = () => {
           essential
         />
         <div className={MarginTop6}>
-          <RegistPhoto className={cx(Flex, Center)}>
-            <Icon {...iconPropsGenerator('regist-photo', '24')} />
-          </RegistPhoto>
+          {imageUrl && (
+            <PostImgContainer>
+              <img
+                src={imageUrl}
+                alt="posted coffee"
+                onTouchEnd={() => {
+                  setImageUrl('');
+                }}
+              />
+            </PostImgContainer>
+          )}
+          {!imageUrl && (
+            <RegistPhoto className={cx(Flex, Center)}>
+              <Icon {...iconPropsGenerator('regist-photo', '24')} />
+              <input
+                ref={fileInputRef}
+                type="file"
+                accept="image/*"
+                onChange={handleInputChange}
+                style={{ display: 'none' }}
+              />
+            </RegistPhoto>
+          )}
         </div>
       </Container>
+
+      {editorEnabled && (
+        <PinturaEditorModal
+          {...editorDefaults}
+          src={editorSrc}
+          imageCropAspectRatio={1.378}
+          onHide={handleEditorHide}
+          onProcess={handleEditorProcess}
+          willRenderCanvas={(shapes, state) => {
+            const { utilVisibility, selectionRect, lineColor } = state;
+
+            if (utilVisibility.crop <= 0) return shapes;
+
+            const { x, y, width, height } = selectionRect;
+
+            return {
+              ...shapes,
+
+              interfaceShapes: [
+                {
+                  x: x + width * 0.5,
+                  y: y + height * 0.5,
+                  opacity: utilVisibility.crop,
+                  inverted: true,
+                  backgroundColor: [0, 0, 0, 0.1],
+                  strokeWidth: 0.4,
+                  strokeColor: [...lineColor]
+                },
+                ...shapes.interfaceShapes
+              ]
+            };
+          }}
+        />
+      )}
+
       <Button
         text={BUTTON_TEXTS.regist}
         onTouchEnd={clickRegisterBtn}
@@ -70,7 +162,7 @@ const MarginTop6 = css`
   margin-top: 6px;
 `;
 
-const RegistPhoto = styled.button`
+const RegistPhoto = styled.label`
   width: 106px;
   height: 106px;
   border-radius: 10px;
@@ -88,8 +180,11 @@ const BtnContainer = css`
   bottom: 0;
 `;
 
-const PostRegisterContainer = styled.div`
-  height: inherit;
+const PostImgContainer = styled.div`
+  max-width: 100%;
+  height: auto;
+  border-radius: 10px;
+  overflow: hidden;
 `;
 
 export default PostRegister;
