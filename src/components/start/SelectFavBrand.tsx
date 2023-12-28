05b1397a3beb5bc054468771747f9080c220c0c2
Siwoo Lee <siwoo28@kakao.com>
2023-12-28 15:44:01 +0900

    Feat: selectFavBrand.tsx 브랜드 데이터 및 로고 추가

--- src/components/start/SelectFavBrand.tsx
+++ src/components/start/SelectFavBrand.tsx
@@ -3,9 +3,11 @@ import { setInitialInfo } from '@/api/user';
 import { authState } from '@/atoms/atoms';
 import BrandItem from '@/components/start/BrandItem';
 import Button from '@/components/common/Button';
-import { SELECTFAVBRAND_TEXTS, BRANDLIST } from '@/constants/start';
+
+import { SELECTFAVBRAND_TEXTS } from '@/constants/start';
 import { BUTTON_TEXTS } from '@/constants/common';
 import { useComposeHeader } from '@/hooks/useComposeHeader';
+import { setBrnadList } from '@/utils/setBrandList';
 import { useImgSubmit } from '@/hooks/useImgSubmit';
 import { useNavigateTo } from '@/hooks/useNavigateTo';
 import { AuthTypes } from '@/types/types';
@@ -23,6 +25,7 @@ export const SelectFavBrand = () => {
 
   const navigateToHome = useNavigateTo('/');
   const navigateToMe = useNavigateTo('/start/3');
+  const brandList = setBrnadList();
 
   const handleStartBtn = () => {
     const userInfo: AuthTypes = {
@@ -43,7 +46,7 @@ export const SelectFavBrand = () => {
     handleFormSubmit();
     navigateToHome();
   };
-
+  console.log(brandList);
   return (
     <>
       <div className={StartPageContainer}>
@@ -53,13 +56,12 @@ export const SelectFavBrand = () => {
           <SecondLine>{message.second}</SecondLine>
         </InitialFormText>
         <BrandItemContainer className={Grid}>
-          {BRANDLIST.map((item, idx) => (
-            <div key={idx}>
-              <BrandItem
-                brand={item.brand}
-                icon={item.icon}
-              />
-            </div>
+          {brandList.map(item => (
+            <BrandItem
+              key={item}
+              brand={item}
+              icon={item}
+            />
           ))}
         </BrandItemContainer>
       </div>
@@ -74,24 +76,24 @@ export const SelectFavBrand = () => {
 };
 
 const InitialFormText = styled.div`
-  margin-top: 28px;
+  margin-top: 1.75rem;
   color: #313131;
   font-size: var(--font-sizes-xxl);
   font-weight: 600;
-  line-height: 32px;
+  line-height: 2rem;
 `;
 
 const SecondLine = styled.span`
   color: #767676;
   font-family: Pretendard;
-  font-size: 16px;
+  font-size: 1rem;
   font-style: normal;
   font-weight: 400;
-  line-height: 24px;
+  line-height: 1.5rem;
 `;
 
 const BrandItemContainer = styled.div`
-  margin: 38px auto 60px;
-  gap: 12px 12px;
+  margin: 2.375rem auto 3.75rem;
+  gap: 0.75rem 0.75rem;
   grid-template-columns: 1fr 1fr 1fr;
 `;
