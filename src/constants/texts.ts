b6a3b170f8ce9f46dcbcb3d6e8ce4c4888eb651b
hookor <gerrard1219@gmail.com>
2024-02-24 03:30:53 +0900

    Chore: post && comments constants

--- src/constants/texts.ts
+++ src/constants/texts.ts
@@ -1,9 +1,16 @@
 export const TEXT = {
-    exitButtonText: '회원 탈퇴',
-    saveButton: '저장하기',
-    addedcaffeine: '누적 카페인',
-    mgLabel: 'mg',
-    deleteBtn: '삭제',
-    toggleFollowedBtn: '팔로워',
-    toggleFollowingBtn: '팔로잉'
-}
+  exitButtonText: '회원 탈퇴',
+  saveButton: '저장하기',
+  addedcaffeine: '누적 카페인',
+  mgLabel: 'mg',
+  deleteBtn: '삭제',
+  toggleFollowedBtn: '팔로워',
+  toggleFollowingBtn: '팔로잉'
+};
+
+export const COMMENT_TEXTS = {
+  count: '개의 댓글',
+  reply: '답글 달기',
+  hide: '답글 숨기기',
+  loadMore: '개의 답글 더보기'
+};
