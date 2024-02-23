47d698139e4300f1637ad14cb4bb87f04b2bd57f
hookor <gerrard1219@gmail.com>
2024-02-24 03:24:22 +0900

    Rename: MiniProfile nickname prop

--- src/components/common/MiniProfile.tsx
+++ src/components/common/MiniProfile.tsx
@@ -1,4 +1,5 @@
 import Icon from '@/components/common/Icon';
+import ImgContainer from '@/components/common/ImgContainer';
 import { TEXT } from '@/constants/texts';
 import { SimplifyUser } from '@/types/types';
 import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
@@ -7,25 +8,38 @@ import { ProfileInfo, ProfileTitle } from '@/styles/styles';
 import { styled } from 'styled-system/jsx';
 import { cx } from 'styled-system/css';
 
-const MiniProfile: React.FC<SimplifyUser> = ({ NickName, caffeine }) => {
+const MiniProfile: React.FC<SimplifyUser> = ({ url, nickname, caffeine }) => {
   return (
-    <Container className={Flex}>
-      <div className={FlexCenter}>
-        <Icon {...iconPropsGenerator('mini-user', '44')} />
-      </div>
-      <div className={cx(Column, Justify)}>
-        <span className={ProfileTitle}>{NickName}</span>
-        <span
-          className={
-            ProfileInfo
-          }>{`${TEXT.addedcaffeine} ${caffeine} ${TEXT.mgLabel}`}</span>
-      </div>
-    </Container>
+    <>
+      {url && (
+        <Container className={Flex}>
+          <div className={FlexCenter}>
+            <ImgContainer url={url} />
+            {/* <Icon {...iconPropsGenerator('mini-user', '44')} /> */}
+          </div>
+          <div className={cx(Column, Justify)}>
+            <UserTitle className={cx(LineH18, Semibold)}>{nickname}</UserTitle>
+            <UserCaffeine
+              className={cx(
+                Flex,
+                LineH18
+              )}>{`${TEXT.addedcaffeine} ${caffeine} ${TEXT.mgLabel}`}</UserCaffeine>
+          </div>
+        </Container>
+      )}
+    </>
   );
 };
 
 const Container = styled.div`
   gap: 12px;
+`;
+const UserTitle = styled.span`
+  font-size: var(--font-sizes-sm);
+`;
+const UserCaffeine = styled.span`
+  font-size: var(--font-sizes-xs);
+  color: #767676;
 `;
 
 export default MiniProfile;
