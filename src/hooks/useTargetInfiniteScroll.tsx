c5906d4b889b241a4ffbe24fe6be1742908c811d
hookor <gerrard1219@gmail.com>
2024-02-28 02:11:35 +0900

    Feat: apply Infinite Scroll on following posts page

---
+++ src/hooks/useTargetInfiniteScroll.tsx
@@ -0,0 +1,22 @@
+import { useIntersection } from '@/hooks/useIntersection';
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
+import { InfinitePosts } from '@/types/types';
+import { useQuery } from '@tanstack/react-query';
+import useGetCacheData from '@/hooks/useGetCacheData';
+
+export const useTargetInfiniteScroll = (param: InfinitePosts) => {
+  const { data: signedIn } = useQuery({
+    queryKey: ['signedIn'],
+    queryFn: () => useGetCacheData('user', '/accessToken')
+  });
+
+  const { data, hasNextPage, isFetching, fetchNextPage, isLoading } =
+    useInfiniteScroll(param, signedIn);
+
+  const ref = useIntersection((entry, observer) => {
+    observer.unobserve(entry.target);
+    if (hasNextPage && !isFetching) fetchNextPage();
+  });
+
+  return { data, ref, isLoading };
+};
