e4f1efd200e902bb1e1fd35c6f8e093b086a21a1
Siwoo Lee <siwoo28@kakao.com>
2024-03-01 23:48:08 +0900

    Feat: React Circular Progressbar

--- src/components/home/CoffeeIntake.tsx
+++ src/components/home/CoffeeIntake.tsx
@@ -1,5 +1,7 @@
-import { DocumentData } from 'firebase/firestore';
-import { CAFFEINE_PER_WATER_TEXTS } from '@/constants/home';
+import {
+  CAFFEINE_PER_WATER_TEXTS,
+  CAFFEINE_INFO_TEXTS
+} from '@/constants/home';
 import { FlexCenter } from '@/styles/layout';
 import {
   CaffeineDetail,
@@ -13,21 +15,35 @@ import { styled } from 'styled-system/jsx';
 import { CoffeeInfo } from '@/types/types';
 
 const { coffeeIntake } = CAFFEINE_PER_WATER_TEXTS;
+const { unit } = CAFFEINE_INFO_TEXTS;
 
 const CoffeeIntake = ({ data }: { data: CoffeeInfo | undefined }) => {
+  const percentage =
+    data?.caffeineSum && data?.caffeineSum / 4 <= 100
+      ? data?.caffeineSum / 4
+      : 100;
+
   return (
     <div>
       <div className={CaffeineDetail}>{coffeeIntake.title}</div>
       <div className={HomeContent}>
-        <span className={HomeContentBigNum}>{data?.caffeineSum}</span>mg
-        <span className={HomeContentNum}> /{data?.allCount}잔</span>
+        <span className={HomeContentBigNum}>{data?.caffeineSum}</span>
+        {unit}
+        <span className={HomeContentNum}>
+          {' '}
+          /{data?.allCount}
+          {coffeeIntake.unit}
+        </span>
       </div>
       <div className={InputByteCheck}>{coffeeIntake.subTitle}</div>
       <div className={FlexCenter}>
         <ProgressBar>
-          <Progress />
+          <Progress style={{ width: `${percentage}%` }} />
         </ProgressBar>
-        <div className={cx(InputByteCheck, recommended)}>400mg</div>
+        <div className={cx(InputByteCheck, recommended)}>
+          {coffeeIntake.recommended}
+          {unit}
+        </div>
       </div>
     </div>
   );
@@ -41,13 +57,14 @@ const ProgressBar = styled.div`
   border-radius: 5px;
   background-color: #f1f1f1;
 `;
+
 const Progress = styled.div`
   position: absolute;
-  width: 40%;
   height: 5px;
   border-radius: 5px;
   background-color: var(--colors-main);
 `;
+
 const recommended = css`
   color: #767676;
 `;
