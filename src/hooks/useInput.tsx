0c1f800a3c6aff1f5f93ea58f33cd64ce6303746
hookor <gerrard1219@gmail.com>
2024-03-10 16:53:44 +0900

    Feat: let common Input compatible with refs

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,15 +1,13 @@
 import { useState, ChangeEvent } from 'react';
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { inputNicknameAlertState, useInputState } from '@/atoms/atoms';
+import { useRecoilState } from 'recoil';
+import { inputNicknameAlertState } from '@/atoms/atoms';
 
 export const useInput = (initialValue: string = '') => {
   const [value, setValue] = useState(initialValue);
   const [isAlert, setIsAlert] = useRecoilState(inputNicknameAlertState);
-  const setInputState = useSetRecoilState(useInputState);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
-    setInputState(e.target.value);
     {
       e.target.value && setIsAlert(false);
     }
