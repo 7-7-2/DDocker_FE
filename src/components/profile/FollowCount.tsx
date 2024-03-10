30e4f4c67eb604d63988d1e52e7326c38be3f32c
Siwoo Lee <siwoo28@kakao.com>
2024-03-10 19:28:28 +0900

    Refactor: FollowCount.tsx

--- src/components/profile/FollowCount.tsx
+++ src/components/profile/FollowCount.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react';
+
+import { getUserFollowCounts } from '@/api/user';
+import { FollowCountProps, UserFollowCountsTypes } from '@/types/types';
 import { useNavigateTo } from '@/hooks/useNavigateTo';
-import { FollowCountProps } from '@/types/types';
+
+import { cx } from 'styled-system/css';
+import { styled } from 'styled-system/jsx';
 import { Center, Column, Flex, MarginAuto } from '@/styles/layout';
 import { Border16, Cursor, RecentSearch, SumType } from '@/styles/styles';
-import { styled } from 'styled-system/jsx';
-import { cx } from 'styled-system/css';
 
-const FollowCount: React.FC<FollowCountProps> = ({ icons }) => {
+const FollowCount = ({ data }: FollowCountProps) => {
   const navigate = useNavigateTo('/FOLLOW');
+  const { userId, postCount } = data;
+  const [userFollowCount, setUserFollowCount] =
+    useState<UserFollowCountsTypes>();
+
+  const getUserFollowCount = async () => {
+    const res = userId && (await getUserFollowCounts(userId));
+    setUserFollowCount(res);
+  };
+
+  useEffect(() => {
+    getUserFollowCount();
+  }, [userId]);
+
+  const count = [
+    { number: postCount, label: '게시물' },
+    { number: userFollowCount?.following, label: '팔로잉' },
+    { number: userFollowCount?.followed, label: '팔로워' }
+  ];
 
   const handleStatClick = (label: string) => {
     label === '게시물'
@@ -16,18 +38,18 @@ const FollowCount: React.FC<FollowCountProps> = ({ icons }) => {
 
   return (
     <Container className={cx(Flex, Cursor, Border16)}>
-      {icons.map((icon, index) => (
+      {count.map((item, index) => (
         <Stat
           key={index}
           className={cx(
             Center,
             Column,
             MarginAuto,
-            index === icons.length - 1 ? 'lastStat' : ''
+            index === count.length - 1 ? 'lastStat' : ''
           )}
-          onTouchEnd={() => handleStatClick(icon.label)}>
-          <StatNumber className={RecentSearch}>{icon.number}</StatNumber>
-          <StatLabel className={SumType}>{icon.label}</StatLabel>
+          onTouchEnd={() => handleStatClick(item.label)}>
+          <StatNumber className={RecentSearch}>{item.number}</StatNumber>
+          <StatLabel className={SumType}>{item.label}</StatLabel>
         </Stat>
       ))}
     </Container>
@@ -37,11 +59,11 @@ const FollowCount: React.FC<FollowCountProps> = ({ icons }) => {
 const Container = styled.div`
   width: auto;
   height: 74px;
-  padding: 20px;
+  padding: auto 0;
   background-color: var(--colors-tertiary);
 `;
 const Stat = styled.div`
-  padding: 0px 39px;
+  flex-grow: 1;
   border-right: 1px solid #dbdbdb;
   &.lastStat {
     border-right: none;
