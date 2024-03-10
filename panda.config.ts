fd974b048b6138e7e3d56c8f2bb4b1f547a783ce
hookor <gerrard1219@gmail.com>
2024-03-10 16:55:12 +0900

    Style: ReplyToPanel texts and icon

--- panda.config.ts
+++ panda.config.ts
@@ -21,7 +21,8 @@ export default defineConfig({
         colors: {
           main: { value: '#FF701E' },
           sub: { value: '#3D2D1E' },
-          tertiary: { value: '#f5f5f5' }
+          tertiary: { value: '#f5f5f5' },
+          subtext: { value: '#a6a6a6' }
         },
         fontSizes: {
           xxs: { value: '10px' },
