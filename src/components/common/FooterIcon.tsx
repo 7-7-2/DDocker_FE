e81117cc94021345880a9afb94fca459853291dc
Siwoo Lee <siwoo28@kakao.com>
2024-03-11 17:09:44 +0900

    Feat: FooterIcon getUserId

--- src/components/common/FooterIcon.tsx
+++ src/components/common/FooterIcon.tsx
@@ -1,24 +1,35 @@
 import { useRecoilState } from 'recoil';
-import { activeState } from '@/atoms/atoms';
+import { useEffect, useState } from 'react';
+
 import Icon from '@/components/common/Icon';
-import { useNavigateTo } from '@/hooks/useNavigateTo';
+import { activeState } from '@/atoms/atoms';
 import useGetCacheData from '@/hooks/useGetCacheData';
+import { useNavigateTo } from '@/hooks/useNavigateTo';
 import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
+
 import { Column, Center } from '@/styles/layout';
 import { FooterTextMedium, FooterTextSelected } from '@/styles/styles';
 import { cx } from 'styled-system/css';
 
-const user = await useGetCacheData('user', '/userInfo');
-const userId = user.cacheData.data.userId;
-
-const routeMap = new Map();
-routeMap.set('home', '/');
-routeMap.set('feed', '/posts');
-routeMap.set('coffee', '/coffee');
-routeMap.set('my', `/profile/${userId}`);
-
 const FooterIcon = ({ icon }: { icon: string }) => {
+  const [userId, setUserId] = useState();
   const [active, setActive] = useRecoilState(activeState);
+
+  const getUserId = async () => {
+    const data = await useGetCacheData('user', '/userInfo');
+    setUserId(data.cacheData.data.userId);
+  };
+
+  useEffect(() => {
+    getUserId();
+  }, []);
+
+  const routeMap = new Map();
+  routeMap.set('home', '/');
+  routeMap.set('feed', '/posts');
+  routeMap.set('coffee', '/coffee');
+  routeMap.set('my', `/profile/${userId}`);
+
   const navigateTo = useNavigateTo(routeMap.get(icon));
 
   const handleTouch = () => {
