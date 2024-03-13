c854fb82cb73d9d8d8e2d28aac0dd59336877891
hookor <gerrard1219@gmail.com>
2024-03-13 17:13:58 +0900

    Feat: getCoffeeIntake for auth user

--- src/components/coffee/MyCoffeeSum.tsx
+++ src/components/coffee/MyCoffeeSum.tsx
@@ -2,29 +2,41 @@ import SumBoard from '@/components/coffee/SumBoard';
 import { COFFEE_TEXTS } from '@/constants/coffee';
 import { cx } from 'styled-system/css';
 import { SumTitle, PaddingT22 } from '@/styles/styles';
+import { useQuery } from '@tanstack/react-query';
+import { getCoffeeIntake } from '@/api/coffee';
 
-const { sum } = COFFEE_TEXTS;
+const { sum, week } = COFFEE_TEXTS;
+const cur = new Date();
+const year = cur.getFullYear();
+const month = cur.getMonth() + 1;
 
 const MyCoffeeSum = () => {
+  const { data: coffeeIntake } = useQuery({
+    queryKey: ['coffeeIntake'],
+    queryFn: getCoffeeIntake
+  });
   return (
     <>
-      <h2 className={cx(SumTitle, PaddingT22)}>{sum}</h2>
-      <SumBoard
-        period="이번 주"
-        coffeeAmount={20}
-        caffeineAmount={3367}
-      />
-      <SumBoard
-        period={12}
-        coffeeAmount={209}
-        caffeineAmount={33677}
-      />
-
-      <SumBoard
-        period={2023}
-        coffeeAmount={1554}
-        caffeineAmount={252646}
-      />
+      {coffeeIntake && coffeeIntake.setRes && (
+        <>
+          <h2 className={cx(SumTitle, PaddingT22)}>{sum}</h2>
+          <SumBoard
+            period={week}
+            coffeeAmount={coffeeIntake.setRes.WEEK[0].CountSum}
+            caffeineAmount={coffeeIntake.setRes.WEEK[0].CaffeineSum}
+          />
+          <SumBoard
+            period={month}
+            coffeeAmount={coffeeIntake.setRes.MONTH[0].CountSum}
+            caffeineAmount={coffeeIntake.setRes.MONTH[0].CaffeineSum}
+          />
+          <SumBoard
+            period={year}
+            coffeeAmount={coffeeIntake.setRes.YEAR[0].CountSum}
+            caffeineAmount={coffeeIntake.setRes.YEAR[0].CaffeineSum}
+          />
+        </>
+      )}
     </>
   );
 };
