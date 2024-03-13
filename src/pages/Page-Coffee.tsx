82460cf4aec0667a6866c8665c3b917c94926722
hookor <gerrard1219@gmail.com>
2024-03-13 23:00:27 +0900

    Feat: Public && Private Coffee SumBoard

--- src/pages/Page-Coffee.tsx
+++ src/pages/Page-Coffee.tsx
@@ -1,11 +1,18 @@
 import MyCoffeeSum from '@/components/coffee/MyCoffeeSum';
 import { useComposeHeader } from '@/hooks/useComposeHeader';
+import useGetCacheData from '@/hooks/useGetCacheData';
+import { useQuery } from '@tanstack/react-query';
 
 const Coffee = () => {
   useComposeHeader(true, '', 'icons');
+  const { data: signedIn } = useQuery({
+    queryKey: ['signedIn'],
+    queryFn: () => useGetCacheData('user', '/accessToken')
+  });
+
   return (
     <>
-      <MyCoffeeSum />
+      <MyCoffeeSum signedIn={signedIn} />
     </>
   );
 };
