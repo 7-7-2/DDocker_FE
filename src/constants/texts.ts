a1b1dc7355702891c7da652993690b5e0cb96bbf
hookor <gerrard1219@gmail.com>
2024-03-16 21:52:32 +0900

    Style: set up for daDailyTrendCard

--- src/constants/texts.ts
+++ src/constants/texts.ts
@@ -23,3 +23,10 @@ export const TABS_TEXTS: TabsText = {
   trend: ['trend', '트렌드'],
   following: ['following', '팔로잉']
 };
+
+export const TREND_TEXTS = {
+  daily: '오늘의 인기 게시글',
+  popular: '의 인기 게시글',
+  latest: '최신순',
+  popularity: '인기순'
+};
