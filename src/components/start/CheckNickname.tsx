6998f75502861664c28b42fa3a8ead108f2a0fd4
Siwoo Lee <siwoo28@kakao.com>
2024-03-17 18:25:16 +0900

    Refactor: CheckNickname.tsx && Input, useInput refactoring

--- src/components/start/CheckNickname.tsx
+++ src/components/start/CheckNickname.tsx
@@ -1,59 +1,57 @@
-import { useState } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { checkNickname } from '@/api/user';
-import {
-  inputNicknameAlertState,
-  authState,
-  useInputState
-} from '@/atoms/atoms';
+import { CheckNicknameState, authState } from '@/atoms/atoms';
 import { Input } from '@/components/common/Input';
 import { Label } from '@/components/common/Label';
 import { INPUT_TEXTS } from '@/constants/common';
 import { LABEL_TEXTS } from '@/constants/common';
+import { useInput } from '@/hooks/useInput';
+import useValidateNickname from '@/hooks/useValidateNickname';
 
 const { nickname } = INPUT_TEXTS.type;
+const { message } = LABEL_TEXTS.nickname;
 
 const CheckNickname = () => {
-  const user = useRecoilValue(authState);
-  const inputValue = useRecoilValue(useInputState);
-  const [isAlert, setIsAlert] = useRecoilState(inputNicknameAlertState);
-  const [isApproval, setIsapproval] = useState(false);
   const [userInit, setUserInit] = useRecoilState(authState);
+  const [isApproval, setIsapproval] = useRecoilState(CheckNicknameState);
+  const { value, onChange: handleChange } = useInput();
+
+  const validate = useValidateNickname(value);
 
   const cilckIdCheckBtn = async () => {
     try {
-      const res = await checkNickname(inputValue);
+      const res = validate && (await checkNickname(value));
       res === 1 ? setIsapproval(false) : setIsapproval(res);
-      setIsAlert(true);
 
-      const selectedNickname = {
-        ...userInit,
-        nickname: inputValue
-      };
-
-      if (isApproval) {
-        isApproval && setUserInit(selectedNickname);
-      }
+      validate
+        ? setUserInit({ ...userInit, nickname: value })
+        : setUserInit({ ...userInit, nickname: '' });
     } catch (error) {
       console.log(error);
     }
   };
 
-  const allertMessage = isApproval
-    ? LABEL_TEXTS.nickname.message.approval
-    : LABEL_TEXTS.nickname.message.disapproval;
+  const allertMessage = !validate
+    ? message.validate
+    : isApproval
+      ? message.approval
+      : isApproval !== null
+        ? message.disapproval
+        : undefined;
 
   return (
     <div>
       <Label
         label={LABEL_TEXTS.nickname.label}
         message={allertMessage}
-        isAlert={isAlert}
-        inputValue={inputValue}
+        isAlert={isApproval}
+        inputValue={value}
       />
       <Input
+        inputValue={value}
         type={nickname.typeName}
         handleEvent={cilckIdCheckBtn}
+        handleChange={handleChange}
       />
     </div>
   );
