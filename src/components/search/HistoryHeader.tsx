cfb890cf069411e13cc716012cf9813699dfa554
hookor <gerrard1219@gmail.com>
2024-03-20 18:38:51 +0900

    Feat: insert and remove search history

---
+++ src/components/search/HistoryHeader.tsx
@@ -0,0 +1,30 @@
+import { SEARCH_TEXTS } from '@/constants/search';
+import { Align, Between } from '@/styles/layout';
+import { RecentSearch, DeleteAllBtn } from '@/styles/styles';
+import { cx } from 'styled-system/css';
+import { styled } from 'styled-system/jsx';
+import { useSetHistory } from '@/hooks/useSetHistory';
+
+const HistoryHeader = () => {
+  const { reset } = useSetHistory();
+  const emptyHistory = () => reset();
+  return (
+    <>
+      <Wrapper className={cx(Align, Between)}>
+        <span className={RecentSearch}>{SEARCH_TEXTS.recentSearch}</span>
+        <span
+          className={DeleteAllBtn}
+          onClick={emptyHistory}>
+          {SEARCH_TEXTS.deleteAllBtn}
+        </span>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.div`
+  margin: 0 0 -10px 0;
+  padding: 20px 0;
+`;
+
+export default HistoryHeader;
---
+++ src/components/search/HistoryList.tsx
@@ -0,0 +1,55 @@
+import { useQuery } from '@tanstack/react-query';
+import useGetCacheData from '@/hooks/useGetCacheData';
+import MiniProfile from '@/components/common/MiniProfile';
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-system/jsx';
+import { Align, Between, Flex } from '@/styles/layout';
+import { cx } from 'styled-system/css';
+import Icon from '@/components/common/Icon';
+import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
+import { useSetHistory } from '@/hooks/useSetHistory';
+import { SimplifyUser } from '@/types/types';
+
+const HistoryList = () => {
+  const { data: cachedHistory } = useQuery({
+    queryKey: ['cachedHistory'],
+    queryFn: () => {
+      return useGetCacheData('search', '/user');
+    }
+  });
+  const navigate = useNavigate();
+  const handleToProfile = (userId: string) => () => {
+    navigate(userId);
+  };
+  const { remove } = useSetHistory();
+  const removeHistory = (user: SimplifyUser) => () => {
+    remove(user);
+  };
+
+  return (
+    <>
+      {cachedHistory &&
+        cachedHistory.cacheData.map((user: any) => (
+          <Container className={cx(Align, Between)}>
+            <MiniProfile
+              key={user.userId}
+              url={user.url}
+              nickname={user.nickname}
+              caffeine={user.caffeine}
+              onClick={handleToProfile(`/profile/${user.userId}`)}
+            />
+            <div
+              className={Flex}
+              onClick={removeHistory(user)}>
+              <Icon {...iconPropsGenerator('cancel', '24')} />
+            </div>
+          </Container>
+        ))}
+    </>
+  );
+};
+const Container = styled.div`
+  margin: 10px 0;
+`;
+
+export default HistoryList;
