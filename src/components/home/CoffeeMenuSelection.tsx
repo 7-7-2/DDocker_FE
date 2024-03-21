46e53391c81e24e910a078fd750de7c7e589637b
Siwoo Lee <siwoo28@kakao.com>
2024-03-21 23:50:27 +0900

    Refactor: CoffeeMenuSelection.tsx

--- src/components/home/CoffeeMenuSelection.tsx
+++ src/components/home/CoffeeMenuSelection.tsx
@@ -1,17 +1,17 @@
 import { useParams } from 'react-router-dom';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 
+import SelectBox from '@/components/common/SelectBox';
 import RegisterLabel from '@/components/post/RegisterLabel';
 import { CAFFEINE_FILTER_TEXTS } from '@/constants/home';
+import useGetCoffeeList from '@/hooks/useGetCoffeeList';
 import { caffeineFilterState, registPostState } from '@/atoms/atoms';
 import { CoffeeDataTypes } from '@/types/types';
-import convertBrandName from '@/utils/convertBrandName';
-import useGetCoffeeList from '@/hooks/useGetCoffeeList';
 
 import { css, cx } from 'styled-system/css';
 import { styled } from 'styled-system/jsx';
 import { SmStyle } from '@/styles/styles';
-import { Column, Flex } from '@/styles/layout';
+import { Column, Grid } from '@/styles/layout';
 
 const { coffeeMenu } = CAFFEINE_FILTER_TEXTS;
 
@@ -63,10 +63,11 @@ const CoffeeMenuSelection = () => {
   const menuList = coffeeData && getMenuList(registInfo.brand);
 
   // 커피 브랜드 선택
-  const selectBrand = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    getMenuList(e.target.value);
-    setRegisterData('brand', e.target.value);
+  const selectBrand = (e: React.TouchEvent<HTMLButtonElement>) => {
+    getMenuList(e.currentTarget.value);
+    setRegisterData('brand', e.currentTarget.value);
     setCaffeineInfo(0);
+    console.log(e.currentTarget.value);
   };
 
   // 선택한 커피 메뉴 정보 조회
@@ -80,115 +81,47 @@ const CoffeeMenuSelection = () => {
   };
 
   // 커피 메뉴 선택
-  const selectMenu = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    getMenuInfo(e.target.value);
-    setRegisterData('menu', e.target.value);
+  const selectMenu = (e: React.TouchEvent<HTMLButtonElement>) => {
+    getMenuInfo(e.currentTarget.value);
+    setRegisterData('menu', e.currentTarget.value);
   };
 
   return (
     <div className={MarginTop}>
       {!register && <span className={SmStyle}>{coffeeMenu.title}</span>}
-      <CoffeeSelectContainer className={cx(register ? Column : Flex)}>
+      <CoffeeSelectContainer className={cx(register ? Column : Grid)}>
         {register && (
           <RegisterLabel
             label={coffeeMenu.brand}
             essential
           />
         )}
-        <SelectBox className={registInfo.brand ? seletedBorder : defaultBorder}>
-          <select
-            className={cx(SelectInput, SmStyle)}
-            name={coffeeMenu.brand}
-            id={coffeeMenu.brand}
-            value={registInfo.brand ? registInfo.brand : coffeeMenu.brand}
-            onChange={selectBrand}>
-            <option
-              value={registInfo.brand ? registInfo.brand : coffeeMenu.brand}
-              disabled
-              defaultValue={coffeeMenu.brand}
-              hidden>
-              {registInfo.brand
-                ? convertBrandName(registInfo.brand)
-                : coffeeMenu.brand}
-            </option>
-            {brandList?.map(item => (
-              <option
-                key={item}
-                value={item}>
-                {convertBrandName(item)}
-              </option>
-            ))}
-          </select>
-        </SelectBox>
-        {register && (
-          <RegisterLabel
-            label={coffeeMenu.menu}
-            essential
-          />
-        )}
-        <SelectBox className={registInfo.menu ? seletedBorder : defaultBorder}>
-          <select
-            className={cx(SelectInput, SmStyle)}
-            name={coffeeMenu.menu}
-            id={coffeeMenu.menu}
-            value={registInfo.menu}
-            onChange={selectMenu}>
-            <option
-              value=""
-              disabled
-              defaultValue={coffeeMenu.menu}
-              hidden>
-              {coffeeMenu.menu}
-            </option>
-            {menuList?.map(item => (
-              <option
-                key={item}
-                value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
-        </SelectBox>
+        <SelectBox
+          value={registInfo.brand}
+          defaultValue={registInfo.brand || coffeeMenu.brand}
+          data={brandList}
+          onTouchEnd={selectBrand}
+        />
+        <SelectBox
+          value={registInfo.menu}
+          defaultValue={coffeeMenu.menu}
+          data={menuList}
+          onTouchEnd={selectMenu}
+        />
       </CoffeeSelectContainer>
     </div>
   );
 };
 
-const SelectBox = styled.div`
-  width: 100%;
-  height: 46px;
-  padding: 12px 16px;
-  border-radius: 10px;
-  border: 1px solid #ccc;
-  font-size: var(--font-sizes-sm);
-  &:focus-within {
-    border: 1px solid var(--colors-main);
-  }
-`;
-
-const SelectInput = css`
-  width: 100%;
-  background-color: transparent;
-  outline: none;
-`;
-
 const CoffeeSelectContainer = styled.div`
+  position: relative;
   margin: 6px 0 8px;
   gap: 8px;
+  grid-template-columns: calc(50% - 4px) calc(50% - 4px);
 `;
 
 const MarginTop = css`
   padding-top: 12px;
-`;
-
-const defaultBorder = css`
-  border: 1px solid #ccc;
-  background-color: #fff;
-`;
-
-const seletedBorder = css`
-  border: 1px solid var(--colors-main);
-  background-color: #fff4ee;
 `;
 
 export default CoffeeMenuSelection;
