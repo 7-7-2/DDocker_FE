1b38fe8c8505a19a5313ee0de6cb97ef946ac70e
Siwoo Lee <siwoo28@kakao.com>
2024-03-22 00:19:16 +0900

    Design: Page-Home.tsx

--- src/components/home/CaffeineFilter.tsx
+++ src/components/home/CaffeineFilter.tsx
@@ -5,17 +5,27 @@ import { BUTTON_TEXTS } from '@/constants/common';
 import { useNavigateTo } from '@/hooks/useNavigateTo';
 import { styled } from 'styled-system/jsx';
 import { RegistBtn } from '@/styles/styles';
+import { useQuery } from '@tanstack/react-query';
+import useGetCacheData from '@/hooks/useGetCacheData';
 
 const CaffeineFilter = () => {
+  const registPage = useNavigateTo('/post/register');
+  const { data: signedIn } = useQuery({
+    queryKey: ['signedIn'],
+    queryFn: () => useGetCacheData('user', '/accessToken')
+  });
+
   return (
     <Container>
       <CoffeeSelection />
       <CaffeineInfo />
-      <Button
-        text={BUTTON_TEXTS.regist}
-        onTouchEnd={useNavigateTo('/post/register')}
-        className={RegistBtn}
-      />
+      {signedIn && (
+        <Button
+          text={BUTTON_TEXTS.regist}
+          onTouchEnd={registPage}
+          className={RegistBtn}
+        />
+      )}
     </Container>
   );
 };
