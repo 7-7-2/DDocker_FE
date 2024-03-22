44c75f3c38e98a143c4004781cda15fdb9b54867
hookor <gerrard1219@gmail.com>
2024-03-23 00:48:51 +0900

    Feat: PostBrandPopular with sortBy

--- src/components/posts/trend/PostsTrend.tsx
+++ src/components/posts/trend/PostsTrend.tsx
@@ -1,13 +1,14 @@
 import PostsBrandPopular from '@/components/posts/trend/PostsBrandPopular';
 import PostsDailyPopular from '@/components/posts/trend/PostsDailyPopular';
 import BrandCarousel from '@/components/posts/trend/carousel/BrandCarousel';
-import { PaddingB20, PaddingT20 } from '@/styles/styles';
+import { PaddingB20, PaddingT20, TrendDivider } from '@/styles/styles';
 import { cx } from 'styled-system/css';
 
 const PostsTrend = () => {
   return (
     <div className={cx(PaddingT20, PaddingB20)}>
       <PostsDailyPopular />
+      <div className={TrendDivider} />
       <BrandCarousel />
       <PostsBrandPopular />
     </div>
