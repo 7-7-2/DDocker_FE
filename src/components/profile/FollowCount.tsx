daad7b7f09e5f8c0f445e00138a70f10798be25e
Siwoo Lee <siwoo28@kakao.com>
2024-03-23 18:55:52 +0900

    Feat: ProFilePage && FollowCount &&  FollowBtn

--- src/components/profile/FollowCount.tsx
+++ src/components/profile/FollowCount.tsx
@@ -1,28 +1,62 @@
 import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-import { getUserFollowCounts } from '@/api/user';
+import Button from '@/components/common/Button';
 import { FollowCountProps, UserFollowCountsTypes } from '@/types/types';
 import { useNavigateTo } from '@/hooks/useNavigateTo';
+import useGetCacheData from '@/hooks/useGetCacheData';
+import { BUTTON_TEXTS } from '@/constants/common';
+import { getUserFollowCounts } from '@/api/user';
+import { checkFollowing, followUser, unfollowUser } from '@/api/follow';
 
-import { cx } from 'styled-system/css';
+import { css, cx } from 'styled-system/css';
 import { styled } from 'styled-system/jsx';
-import { Center, Column, Flex, MarginAuto } from '@/styles/layout';
-import { Border16, Cursor, RecentSearch, SumType } from '@/styles/styles';
+import { Center, Column, Flex } from '@/styles/layout';
+import {
+  Border16,
+  Cursor,
+  FollowBtn,
+  FollowingBtn,
+  RecentSearch,
+  SumType
+} from '@/styles/styles';
 
+const { following, follow1 } = BUTTON_TEXTS;
 const FollowCount = ({ data }: FollowCountProps) => {
   const navigate = useNavigateTo('/FOLLOW');
-  const { userId, postCount } = data;
+  const { userId: ProfileId, postCount } = data;
+  const [isFollowing, setIsFollowing] = useState(true);
   const [userFollowCount, setUserFollowCount] =
     useState<UserFollowCountsTypes>();
 
+  const { data: userInfo } = useQuery({
+    queryKey: ['userInfo'],
+    queryFn: () => useGetCacheData('user', '/userInfo')
+  });
+
+  const userId = userInfo && userInfo.cacheData.data.userId;
+
   const getUserFollowCount = async () => {
-    const res = userId && (await getUserFollowCounts(userId));
+    const res = ProfileId && (await getUserFollowCounts(ProfileId));
     setUserFollowCount(res);
+  };
+
+  const getCheckFollowing = async () => {
+    const res = ProfileId && (await checkFollowing(ProfileId));
+    setIsFollowing(res.data);
+    console.log(isFollowing, res.data);
+  };
+
+  const handleFollowBtn = async () => {
+    !isFollowing && ProfileId && (await followUser(ProfileId));
+    isFollowing && ProfileId && (await unfollowUser(ProfileId));
+    await getCheckFollowing();
   };
 
   useEffect(() => {
     getUserFollowCount();
-  }, [userId]);
+    getCheckFollowing();
+  }, [userId, isFollowing]);
 
   const count = [
     { number: postCount, label: '게시물' },
@@ -37,31 +71,44 @@ const FollowCount = ({ data }: FollowCountProps) => {
   };
 
   return (
-    <Container className={cx(Flex, Cursor, Border16)}>
-      {count.map((item, index) => (
-        <Stat
-          key={index}
-          className={cx(
-            Center,
-            Column,
-            MarginAuto,
-            index === count.length - 1 ? 'lastStat' : ''
-          )}
-          onTouchEnd={() => handleStatClick(item.label)}>
-          <StatNumber className={RecentSearch}>{item.number}</StatNumber>
-          <StatLabel className={SumType}>{item.label}</StatLabel>
-        </Stat>
-      ))}
+    <Container className={cx(Column, Cursor, Border16)}>
+      <CountContainer className={Flex}>
+        {count.map((item, index) => (
+          <Stat
+            key={index}
+            className={cx(
+              Center,
+              Column,
+              index === count.length - 1 ? 'lastStat' : ''
+            )}
+            onTouchEnd={() => handleStatClick(item.label)}>
+            <StatNumber className={RecentSearch}>{item.number}</StatNumber>
+            <StatLabel className={SumType}>{item.label}</StatLabel>
+          </Stat>
+        ))}
+      </CountContainer>
+      {userId !== ProfileId && (
+        <Button
+          text={isFollowing ? following : follow1}
+          onTouchEnd={handleFollowBtn}
+          className={cx(MarginTop, isFollowing ? FollowingBtn : FollowBtn)}
+        />
+      )}
     </Container>
   );
 };
 
 const Container = styled.div`
   width: auto;
-  height: 74px;
-  padding: auto 0;
   background-color: var(--colors-tertiary);
+  padding: 16px;
 `;
+
+const CountContainer = styled.div`
+  height: 42px;
+  margin: 0 -16px;
+`;
+
 const Stat = styled.div`
   flex-grow: 1;
   border-right: 1px solid #dbdbdb;
@@ -69,10 +116,16 @@ const Stat = styled.div`
     border-right: none;
   }
 `;
+
 const StatNumber = styled.li`
   list-style-type: none;
 `;
+
 const StatLabel = styled.li`
   list-style-type: none;
+`;
+
+const MarginTop = css`
+  margin-top: 14px;
 `;
 export default FollowCount;
