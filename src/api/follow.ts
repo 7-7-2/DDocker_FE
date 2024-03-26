9dceef3d058883baff9c188fdd62929c525721f4
hookor <gerrard1219@gmail.com>
2024-03-26 14:52:45 +0900

    Fix: API common error handling

--- src/api/follow.ts
+++ src/api/follow.ts
@@ -1,27 +1,37 @@
-import { authInstance } from "@/api/axiosInterceptor";
+import { authInstance } from '@/api/axiosInterceptor';
 
 // 1. 유저 팔로우
 export const followUser = async (userId: string) => {
-  const res = await authInstance.post(`/follow/${userId}`);
-  return res.data;
+  const res = await authInstance.post(`/follow/${userId}`).catch(e => {
+    console.log(e);
+  });
+  return res && res.data;
 };
 // 2. 유저 언팔로우
 export const unfollowUser = async (userId: string) => {
-  const res = await authInstance.delete(`/follow/${userId}`);
-  return res.data;
+  const res = await authInstance.delete(`/follow/${userId}`).catch(e => {
+    console.log(e);
+  });
+  return res && res.data;
 };
 // 3. 유저의 팔로잉 목록 확인
 export const getFollowingList = async (userId: string) => {
-  const res = await authInstance.get(`/follow/${userId}/following`);
-  return res.data;
+  const res = await authInstance.get(`/follow/${userId}/following`).catch(e => {
+    console.log(e);
+  });
+  return res && res.data;
 };
 // 4. 유저의 팔로우 목록 확인
 export const getFollowerList = async (userId: string) => {
-  const res = await authInstance.get(`/follow/${userId}/follower`);
-  return res.data;
+  const res = await authInstance.get(`/follow/${userId}/follower`).catch(e => {
+    console.log(e);
+  });
+  return res && res.data;
 };
 // 5. 프로필 진입시 팔로잉 중인 유저인지 확인
 export const checkFollowing = async (userId: string) => {
-  const res = await authInstance.get(`/follow/${userId}`);
-  return res.data;
+  const res = await authInstance.get(`/follow/${userId}`).catch(e => {
+    console.log(e);
+  });
+  return res && res.data;
 };
--- src/api/likes.ts
+++ src/api/likes.ts
@@ -2,16 +2,22 @@ import { authInstance } from '@/api/axiosInterceptor';
 
 // 1. 게시글 좋아요
 export const likePost = async (postId: string) => {
-  const res = await authInstance.post(`/likes/${postId}`);
-  return res.data;
+  const res = await authInstance.post(`/likes/${postId}`).catch(e => {
+    console.log(e);
+  });
+  return res && res.data;
 };
 // 2. 게시글 좋아요 취소
 export const undoLikePost = async (postId: string) => {
-  const res = await authInstance.delete(`/likes/${postId}`);
-  return res.data;
+  const res = await authInstance.delete(`/likes/${postId}`).catch(e => {
+    console.log(e);
+  });
+  return res && res.data;
 };
 // 3. 인증된 사용자가 게시글의 본인 좋아요 확인(게시글 진입시)
 export const getMyLikeOnPost = async (postId: string) => {
-  const res = await authInstance.get(`/likes/${postId}`);
-  return res.data;
+  const res = await authInstance.get(`/likes/${postId}`).catch(e => {
+    console.log(e);
+  });
+  return res && res.data;
 };
