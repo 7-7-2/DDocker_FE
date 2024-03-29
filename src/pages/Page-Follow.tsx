2b5e7de7dedeac40ea7b52f10cd578446ef4b8ea
hookor <gerrard1219@gmail.com>
2024-03-29 21:58:18 +0900

    Feat: rescaffold follow page

--- src/pages/Page-Follow.tsx
+++ src/pages/Page-Follow.tsx
@@ -1,109 +1,48 @@
 import { useState } from 'react';
-import EmptyUser from '@/components/follow/EmptyUser';
 import UserListItem from '@/components/follow/UserListItem';
-import { TEXT } from '@/constants/texts';
-import { useComposeHeader } from '@/hooks/useComposeHeader';
-import { useNavigateTo } from '@/hooks/useNavigateTo';
-import { FollowCountProps, SimplifyUser } from '@/types/types';
-import { Column, FlexCenter, Flex } from '@/styles/layout';
-import {
-  TextGray,
-  ToggleButton,
-  ToggleLeft,
-  ToggleRight,
-  MarginT17
-} from '@/styles/styles';
-import { styled } from 'styled-system/jsx';
-import { cx } from 'styled-system/css';
+import { FollowCountProps } from '@/types/types';
+import { getUsernameById } from '@/api/follow';
+import { useLocation, useParams } from 'react-router-dom';
+import { useGetSignedIn } from '@/hooks/useGetSignedIn';
+import { useQuery } from '@tanstack/react-query';
+import FollowTabs from '@/components/follow/FollowTabs';
 
 const Follow: React.FC<FollowCountProps> = () => {
-  useComposeHeader(false, '커피좋아', '');
-  const [isActiveBtn, setIsActiveBtn] = useState('팔로워');
+  const { signedIn } = useGetSignedIn();
+  const { userId } = useParams();
 
-  const handleButtonClick = (e: React.TouchEvent<HTMLButtonElement>) => {
+  const { state: tabState } = useLocation();
+  const [activeTab, setActiveTab] = useState(tabState || '팔로워');
+
+  const { data: username } = useQuery({
+    queryKey: ['username', userId],
+    queryFn: () => {
+      return userId && getUsernameById(userId);
+    },
+    enabled: !!userId && !!signedIn
+  });
+
+  const handleButtonTouch = (e: React.TouchEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setIsActiveBtn(e.currentTarget?.value || '팔로워');
+    setActiveTab(e.currentTarget?.value);
   };
 
-  const followers: SimplifyUser[] = [
-    {
-      userId: 'asd',
-      nickname: '커피마셔마셔',
-      caffeine: 34054
-    },
-    {
-      userId: 'zxc',
-      nickname: '커피안마셔안마셔',
-      caffeine: 12345
-    }
-  ];
-
-  const followings: SimplifyUser[] = [
-    {
-      userId: 'zxc',
-      nickname: '커피안마셔안마셔',
-      caffeine: 12345
-    },
-    {
-      userId: 'asd',
-      nickname: '커피마셔마셔',
-      caffeine: 34054
-    }
-  ];
-
-  const usersToShow = isActiveBtn === '팔로워' ? followers : followings;
-
-  //   const userId: string = 'exampleUserId'; // useParams 를 위한 것
-
-  const handleMoveBtn = useNavigateTo('/search');
   return (
     <>
-      <div className={cx(FlexCenter, Column, MarginT17)}>
-        <ToggleArea className={cx(Flex)}>
-          <button
-            className={cx(
-              ToggleButton,
-              FlexCenter,
-              TextGray,
-              ToggleLeft,
-              isActiveBtn === '팔로워' && 'active'
-            )}
-            onTouchEnd={handleButtonClick}
-            value="팔로워">
-            {TEXT.toggleFollowedBtn}
-          </button>
-          <button
-            className={cx(
-              ToggleButton,
-              FlexCenter,
-              TextGray,
-              ToggleRight,
-              isActiveBtn === '팔로잉' && 'active'
-            )}
-            onTouchEnd={handleButtonClick}
-            value="팔로잉">
-            {TEXT.toggleFollowingBtn}
-          </button>
-        </ToggleArea>
-      </div>
-
-      {usersToShow.length > 0 && <UserListItem users={usersToShow} />}
-      {isActiveBtn === '팔로워' && !usersToShow.length && (
-        <EmptyUser label="팔로워" />
-      )}
-      {isActiveBtn === '팔로잉' && !usersToShow.length && (
-        <EmptyUser
-          label="팔로잉"
-          onTouchEnd={handleMoveBtn}
+      {username && (
+        <FollowTabs
+          activeTab={activeTab}
+          handleButtonTouch={handleButtonTouch}
+          username={username.data}
         />
       )}
+
+      <UserListItem
+        activeTab={activeTab}
+        pageUserId={userId}
+      />
     </>
   );
 };
-
-const ToggleArea = styled.div`
-  width: 100%;
-  padding-bottom: 4px;
-`;
 
 export default Follow;
