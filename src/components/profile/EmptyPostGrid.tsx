5b1503ef481f04f1fc558c917050afcaa7ca166f
Siwoo Lee <siwoo28@kakao.com>
2024-04-01 15:29:18 +0900

    Feat: page-Profile && EmptyPostGrid.tsx && CTA

---
+++ src/components/profile/EmptyPostGrid.tsx
@@ -0,0 +1,49 @@
+import CTA from '@/components/common/CTA';
+import { PROFILE_TEXTS } from '@/constants/profile';
+
+import { css, cx } from 'styled-system/css';
+import { Justify } from '@/styles/layout';
+import { styled } from 'styled-system/jsx';
+
+const {
+  user: { my, another }
+} = PROFILE_TEXTS;
+
+const EmptyPostGrid = ({
+  profileId,
+  userId
+}: {
+  profileId: string | undefined;
+  userId?: string;
+}) => {
+  return (
+    <Container
+      className={cx(
+        Justify,
+        userId || (userId === profileId && DefaultHeight)
+      )}>
+      {userId === profileId ? (
+        <CTA
+          text={my.text}
+          actionText={my.actionText}
+        />
+      ) : (
+        <CTA
+          text={another}
+          btn={false}
+        />
+      )}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: auto 0;
+  height: calc(100vh - 460px);
+`;
+
+const DefaultHeight = css`
+  height: calc(100vh - 500px);
+`;
+
+export default EmptyPostGrid;
