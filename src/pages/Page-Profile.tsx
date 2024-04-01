f13264b1cb8c5c950473ee77ef820636ba73a1c4
Siwoo Lee <siwoo28@kakao.com>
2024-04-01 16:08:18 +0900

    Refactor: Page-Profile.tsx && FollowCount, PostsGrid

--- src/pages/Page-Profile.tsx
+++ src/pages/Page-Profile.tsx
@@ -1,69 +1,62 @@
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
+import ProfileDetail from '@/components/profile/ProfileDetail';
 import FollowCount from '@/components/profile/FollowCount';
 import PostsGrid from '@/components/profile/PostsGrid';
-import ProfileDetail from '@/components/profile/ProfileDetail';
-import CTA from '@/components/common/CTA';
-import { InfinitePosts, UserProfileDataTypes } from '@/types/types';
+import EmptyPostGrid from '@/components/profile/EmptyPostGrid';
+import AnonymousUserCard from '@/components/profile/AnonymousUserCard';
+
+import { UserProfileDataTypes } from '@/types/types';
 import { useTargetInfiniteScroll } from '@/hooks/useTargetInfiniteScroll';
 import { getProfilePostIQParam } from '@/hooks/useInfiniteScroll';
 import { useComposeHeader } from '@/hooks/useComposeHeader';
-import { useNavigateTo } from '@/hooks/useNavigateTo';
-import { PROFILE_TEXTS } from '@/constants/profile';
+import { useCachedUserInfo } from '@/hooks/useCachedUserInfo';
 
-import { Default, Semibold } from '@/styles/styles';
-import { Between, Column, Justify } from '@/styles/layout';
+import { Between, Column } from '@/styles/layout';
 import { styled } from 'styled-system/jsx';
 import { cx } from 'styled-system/css';
 
-const {
-  anonymous: { text, actionText, title }
-} = PROFILE_TEXTS;
-
 const Profile = () => {
   useComposeHeader(true, '', 'icons');
-  const { userId } = useParams();
-  const goToSignIn = useNavigateTo('/start/1');
+  const { userId: profileId } = useParams();
+  const { userId } = useCachedUserInfo();
 
-  const handleSignInBtn = () => {
-    goToSignIn();
-  };
-
-  if (userId === 'Non-members') {
-    const anonymousUser = (
-      <AnonymousCard className={cx(Default, Column, Justify)}>
-        <Title className={Semibold}>{title}</Title>
-        <CTA
-          text={text}
-          actionText={actionText}
-          btn
-          fn={handleSignInBtn}
-        />
-      </AnonymousCard>
-    );
-
-    return <div>{anonymousUser}</div>;
-  }
-
-  const ProfilePostIQParam: InfinitePosts = getProfilePostIQParam();
-  const { data, ref } = useTargetInfiniteScroll(ProfilePostIQParam);
-  const postsData = data as unknown as UserProfileDataTypes[];
+  const ProfilePostIQParam = getProfilePostIQParam();
+  const { data, ref: postRef } = useTargetInfiniteScroll(ProfilePostIQParam);
+  const postsData = data && (data as unknown as UserProfileDataTypes[]);
+  const allCount = postsData && postsData[0].allCount;
 
   const followCountData = {
-    userId: userId,
+    userId: profileId,
     postCount: postsData && postsData[0].allCount
   };
 
+  const postGrid = allCount ? (
+    <PostsGrid
+      data={postsData}
+      postRef={postRef}
+    />
+  ) : (
+    <EmptyPostGrid
+      profileId={profileId}
+      userId={userId}
+    />
+  );
+
   return (
     <div>
-      <Container className={Column}>
-        <div className={cx(Column, Between)}>
-          <ProfileDetail userId={userId} />
-          <FollowCount data={followCountData} />
-        </div>
-      </Container>
-      <PostsGrid data={postsData} />
-      <Target ref={ref} />
+      {profileId !== 'Non-members' ? (
+        <Container className={Column}>
+          <div className={cx(Column, Between)}>
+            <ProfileDetail userId={profileId} />
+            <FollowCount data={followCountData} />
+          </div>
+          {profileId && postGrid}
+        </Container>
+      ) : (
+        <AnonymousUserCard />
+      )}
     </div>
   );
 };
@@ -71,23 +64,8 @@ const Profile = () => {
 const Container = styled.div`
   position: relative;
   width: auto;
-  margin: 20px 0;
-`;
-const Target = styled.div`
-  width: 1px;
-`;
-
-const AnonymousCard = styled.div`
-  width: inherit;
-  height: 364px;
-  border-radius: 16px;
-  margin-top: 28px;
-  padding: 0 52px;
-`;
-const Title = styled.div`
-  margin-bottom: 8px;
-  font-size: 16px;
-  line-height: 22px;
+  margin-top: 20px;
+  gap: 20px;
 `;
 
 export default Profile;
