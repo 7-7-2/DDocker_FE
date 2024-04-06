addb9355f413455ec43fc39edd460139277a376e
Siwoo Lee <siwoo28@kakao.com>
2024-04-06 14:12:40 +0900

    Feat: scroll inducer.tsx

---
+++ src/components/coffee/ScrollInducer.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import { SCROLL_INDUCER_TEXTS } from '@/constants/common';
+import { Regular } from '@/styles/styles';
+import { styled } from 'styled-system/jsx';
+import { css, cx } from 'styled-system/css';
+import { Justify } from '@/styles/layout';
+
+const { text } = SCROLL_INDUCER_TEXTS;
+const ScrollInducer = ({
+  targetRef
+}: {
+  targetRef: React.RefObject<HTMLDivElement>;
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        setIsVisible(entry.isIntersecting);
+      });
+    });
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => {
+      if (targetRef.current) {
+        observer.unobserve(targetRef.current);
+      }
+    };
+  }, [targetRef]);
+
+  return (
+    <Container className={isVisible ? Default : undefined}>
+      <Text className={cx(Regular, Justify)}>{text}</Text>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 80px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 47px;
+  background-image: linear-gradient(#f5f5f530, #e9e9e9df);
+  opacity: 100;
+  transition: opacity 0.5s ease;
+`;
+
+const Default = css`
+  opacity: 0;
+  transition: opacity 0.5s ease;
+`;
+
+const Text = styled.div`
+  width: 100%;
+  margin-top: 40px;
+  font-size: var(--font-sizes-sm);
+  color: var(--colors-mid-grey);
+`;
+
+export default ScrollInducer;
