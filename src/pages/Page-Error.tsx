c7a2491eae12909b59831afd888f4a4551f5c73a
Siwoo Lee <siwoo28@kakao.com>
2024-04-16 00:23:42 +0900

    Feat: Page-Error

--- src/pages/Page-Error.tsx
+++ src/pages/Page-Error.tsx
@@ -1,5 +1,65 @@
+import Button from '@/components/common/Button';
+import { BUTTON_TEXTS } from '@/constants/common';
+import { ERROR_TEXTS } from '@/constants/error';
+import { useNavigateTo } from '@/hooks/useNavigateTo';
+
+import { Align, Column, MarginAuto } from '@/styles/layout';
+import { DefaultBtn, Regular, Semibold } from '@/styles/styles';
+import { cx } from 'styled-system/css';
+import { styled } from 'styled-system/jsx';
+
+const { home } = BUTTON_TEXTS;
+const { title, text } = ERROR_TEXTS;
 const Error = () => {
-  return <>Page-Error</>;
+  return (
+    <Container className={cx(Align, Column)}>
+      <ContentsContainer>
+        <svg
+          width={'158'}
+          height={'136'}
+          className={MarginAuto}>
+          <use href={`/sprite.svg#icon-ddocker-error`} />
+        </svg>
+        <Title className={Semibold}>{title}</Title>
+        <Text className={Regular}>{text}</Text>
+      </ContentsContainer>
+      <Button
+        text={home}
+        className={cx(DefaultBtn)}
+        onTouchEnd={useNavigateTo('/')}
+      />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  position: relative;
+  max-width: 500px;
+  min-width: 360px;
+  width: 100vw;
+  height: 100vh;
+  padding: 0 20px 20px;
+  margin-bottom: calc(env(safe-area-inset-bottom));
+`;
+
+const ContentsContainer = styled.div`
+  text-align: center;
+  margin: auto 0;
+`;
+
+const Title = styled.div`
+  margin-top: 26px;
+  font-size: var(--font-sizes-xl);
+  color: var(--colors-main-dark);
+  line-height: 24px;
+`;
+
+const Text = styled.div`
+  margin: 10px auto 0;
+  white-space: break-spaces;
+  text-align: center;
+  color: var(--colors-mid-grey);
+  line-height: 20px;
+`;
 
 export default Error;
