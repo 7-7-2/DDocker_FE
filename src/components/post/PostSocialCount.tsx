314b18a619c3d1f361e40112ff1f549de6cd0be2
hookor <gerrard1219@gmail.com>
2024-04-15 17:25:22 +0900

    Feat: keyframes animation on undolike and like

--- src/components/post/PostSocialCount.tsx
+++ src/components/post/PostSocialCount.tsx
@@ -1,6 +1,7 @@
 import Icon from '@/components/common/Icon';
 import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
 import { styled } from 'styled-system/jsx';
+import { css } from 'styled-system/css';
 import { Align } from '@/styles/layout';
 import { CaffeineDetail } from '@/styles/styles';
 
@@ -15,10 +16,14 @@ const PostSocialCount = ({
 }) => {
   return (
     <Container className={Align}>
-      {}
-      <Icon
-        {...iconPropsGenerator(icon)}
-        onClick={onClick}></Icon>
+      <div
+        className={icon.includes('like') ? Transition : ''}
+        onAnimationStart={onClick}>
+        <Icon
+          {...iconPropsGenerator(icon)}
+          onClick={onClick}
+        />
+      </div>
       <Count className={CaffeineDetail}>{count}</Count>
     </Container>
   );
@@ -33,6 +38,13 @@ const Count = styled.span`
   width: 22px;
   line-height: 22px;
   align-self: end;
+`;
+
+const Transition = css`
+  &:active {
+    animation-name: like;
+    animation-duration: 2s;
+  }
 `;
 
 export default PostSocialCount;
