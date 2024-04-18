fff4f3ac1ac42ed20286bda55e33245378dc139f
hookor <gerrard1219@gmail.com>
2024-04-18 16:45:30 +0900

    Feat: apply scrollToTop on new postDetail load

--- src/hooks/post/useRefIntoView.ts
+++ src/hooks/post/useRefIntoView.ts
@@ -1,11 +1,14 @@
 import { useEffect, useRef } from 'react';
 
-export const useRefIntoView = (dep: any) => {
+export const useRefIntoView = (
+  dep: any,
+  action = 'smooth' as ScrollBehavior
+) => {
   const ref = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     if (ref && ref.current) {
       ref.current.scrollIntoView({
-        behavior: 'smooth',
+        behavior: action,
         block: 'center',
         inline: 'center'
       });
