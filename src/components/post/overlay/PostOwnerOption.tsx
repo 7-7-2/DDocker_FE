1f5d918ada427bbe1359af0c863d26fb4747b91e
Siwoo Lee <siwoo28@kakao.com>
2024-04-25 20:05:17 +0900

    Feat: updatePost

--- src/components/post/overlay/PostOwnerOption.tsx
+++ src/components/post/overlay/PostOwnerOption.tsx
@@ -1,7 +1,6 @@
 import { BackgroundLayer } from '@/components/post/overlay/BackgroundLayer';
 import { PostOptions } from '@/components/post/overlay/PostOptions';
 import { Option } from '@/components/post/overlay/Option';
-import { useMutation } from '@tanstack/react-query';
 import { useNavigateTo } from '@/hooks/useNavigateTo';
 import { useSetRecoilState } from 'recoil';
 import { footerShowState } from '@/atoms/atoms';
@@ -16,6 +15,7 @@ const PostOwnerOption = ({
   setConfirm: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }) => {
   const setFooterState = useSetRecoilState(footerShowState);
+  const goToUpdate = useNavigateTo(`/post/${postId}/update`);
 
   const confirmDelete = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault();
@@ -26,7 +26,7 @@ const PostOwnerOption = ({
 
   //등록 페이지 수정시 postId 전달 혹은 수정 페이지로 이동
   const handleUpdate = () => {
-    console.log(postId);
+    goToUpdate();
   };
 
   return (
