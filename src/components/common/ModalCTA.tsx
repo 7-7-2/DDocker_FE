a1144959b20faac84256f99778a044e382b9df5a
Siwoo Lee <siwoo28@kakao.com>
2024-04-29 14:41:36 +0900

    Feat: deleteUserAccount confirm Modal

--- src/components/common/ModalCTA.tsx
+++ src/components/common/ModalCTA.tsx
@@ -8,9 +8,9 @@ import { MODAL_CTA_TEXTS } from '@/constants/common';
 import { isModalState } from '@/atoms/atoms';
 import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
 
-import { Align, Between, Column } from '@/styles/layout';
+import { Align, Between, Column, Flex } from '@/styles/layout';
 import { Medium, RegistBtn } from '@/styles/styles';
-import { cx } from 'styled-system/css';
+import { css, cx } from 'styled-system/css';
 import { styled } from 'styled-system/jsx';
 
 const { signIn } = MODAL_CTA_TEXTS;
@@ -18,33 +18,67 @@ const { signIn } = MODAL_CTA_TEXTS;
 const ModalCTA = ({
   actionText,
   text,
+  subBtnText,
+  isConfirm,
   fn: handleActions
 }: {
   actionText: string;
   text: string;
+  subBtnText?: string;
+  isConfirm?: boolean;
   fn: React.TouchEventHandler<HTMLButtonElement>;
 }) => {
   const [isModal, setIsModal] = useRecoilState(isModalState);
-  const Handleonclick = () => {
-    setIsModal(!isModal);
+
+  const HandleOnclick = () => {
+    isModal && setIsModal(!isModal);
   };
 
+  const confirmDeleteAuth = (
+    <ConfirmContainer className={cx(Flex, Between)}>
+      <Button
+        text={'돌아가기'}
+        onTouchEnd={HandleOnclick}
+        className={cx(RegistBtn, BackBtn)}
+      />
+      <Button
+        text={'탈퇴하기'}
+        onTouchEnd={handleActions}
+        className={RegistBtn}
+      />
+    </ConfirmContainer>
+  );
+
+  const DefalutBtn = (
+    <Button
+      text={actionText}
+      onTouchEnd={handleActions}
+      className={RegistBtn}
+    />
+  );
+
   return (
-    <BackgroundLayer onClick={Handleonclick}>
-      <ModalContainer>
-        <Container className={cx(Column, Between)}>
-          <TextContainer className={cx(Column, Align)}>
-            <Icon {...iconPropsGenerator('ddocker-modal', '60')} />
-            <Text className={Medium}>{text}</Text>
-          </TextContainer>
-          <Button
-            text={actionText}
-            onTouchEnd={handleActions}
-            className={RegistBtn}></Button>
-        </Container>
-        <SubBtn onTouchEnd={Handleonclick}>{signIn.subBtn}</SubBtn>
-      </ModalContainer>
-    </BackgroundLayer>
+    isModal && (
+      <BackgroundLayer onClick={HandleOnclick}>
+        <ModalContainer>
+          <Container className={cx(Column, Between, Align)}>
+            <TextContainer
+              className={cx(
+                Column,
+                Align,
+                !isConfirm ? OnelineText : undefined
+              )}>
+              <Icon {...iconPropsGenerator('ddocker-modal', '60')} />
+              <Text className={Medium}>{text}</Text>
+            </TextContainer>
+            {!isConfirm ? DefalutBtn : confirmDeleteAuth}
+          </Container>
+          <SubBtn onTouchEnd={HandleOnclick}>
+            {subBtnText || signIn.subBtn}
+          </SubBtn>
+        </ModalContainer>
+      </BackgroundLayer>
+    )
   );
 };
 
@@ -61,16 +95,19 @@ const ModalContainer = styled.div`
 `;
 
 const Container = styled.div`
-  position: absolute;
   width: 300px;
-  height: 218px;
+  height: 216px;
   padding: 16px;
   border-radius: 16px;
   background-color: #ffffff;
 `;
 
 const TextContainer = styled.div`
-  gap: 2px;
+  gap: 6px;
+`;
+
+const OnelineText = css`
+  gap: 16px;
 `;
 
 const Text = styled.div`
@@ -86,6 +123,17 @@ const SubBtn = styled.button`
   text-decoration: underline;
   bottom: 0;
   font-size: var(--font-sizes-sm);
+`;
+
+const ConfirmContainer = styled.div`
+  width: 100%;
+  gap: 8px;
+`;
+
+const BackBtn = css`
+  color: var(--colors-mid-grey);
+  background-color: #fff;
+  border: 1px solid var(--colors-btn-grey);
 `;
 
 export default ModalCTA;
