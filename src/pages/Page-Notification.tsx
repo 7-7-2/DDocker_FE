a46091b3c59b710c3fc1a6c6b618fa2f7966948a
hookor <gerrard1219@gmail.com>
2024-05-14 02:35:27 +0900

    Feat: display no notifications on empty list

--- src/pages/Page-Notification.tsx
+++ src/pages/Page-Notification.tsx
@@ -1,11 +1,16 @@
+import CTA from '@/components/common/CTA';
 import NoticeItem from '@/components/notification/NoticeItem';
+import { CTA_TEXTS } from '@/constants/texts';
 import { useComposeHeader } from '@/hooks/useComposeHeader';
 import useGetCacheData from '@/hooks/useGetCacheData';
 import { useGetSignedIn } from '@/hooks/useGetSignedIn';
 import useSetCacheData from '@/hooks/useSetCacheData';
+import { FlexCenter } from '@/styles/layout';
+import { AlignCTA, NotificationContainer } from '@/styles/styles';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useEffect, useId } from 'react';
+import { cx } from 'styled-system/css';
 import { styled } from 'styled-system/jsx';
 
 const SignInCTA = React.lazy(
@@ -29,7 +34,8 @@ const Notification = () => {
   }, []);
 
   return (
-    <Container>
+    <Container
+      className={signedIn && !cachedNotification && NotificationContainer}>
       {signedIn &&
         cachedNotification &&
         cachedNotification.cacheData.map((notification: any, idx: number) => (
@@ -42,6 +48,11 @@ const Notification = () => {
             postId={notification.postId || ''}
           />
         ))}
+      {signedIn && !cachedNotification && (
+        <div className={cx(AlignCTA, FlexCenter)}>
+          <CTA text={CTA_TEXTS.emptyNotification} />
+        </div>
+      )}
       {!signedIn && <SignInCTA location="notice" />}
     </Container>
   );
