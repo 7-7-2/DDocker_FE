9503100a34ccad1073678c42cb0b3604944e09f1
Siwoo Lee <siwoo28@kakao.com>
2024-05-13 18:06:53 +0900

    Feat: PostBody | PostsGrid && ImageErrorCTA

--- src/components/profile/PostsGrid.tsx
+++ src/components/profile/PostsGrid.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import ImageErrorCTA from '@/components/common/ImageErrorCTA';
+import { useImgErrorCTA } from '@/hooks/useImgErrorCTA';
 import { PostsGridProps, UserProfilePostsTypes } from '@/types/types';
 
 import { cx } from 'styled-system/css';
@@ -10,30 +12,52 @@ const PostsGrid = ({ data, postRef }: PostsGridProps) => {
   const navigate = useNavigate();
   const posts = data && data.flatMap(item => item.posts);
 
+  const { isError, refresh, handleImgError } = useImgErrorCTA();
+
   const touchImg: React.TouchEventHandler<HTMLImageElement> = (
     e: React.TouchEvent<HTMLImageElement>
   ) => {
     navigate(`/post/${e.currentTarget.id}`);
   };
 
+  const ImgErrorCTA = (
+    <Container>
+      <ImageErrorCTA
+        text={'게시물을 불러올 수 없습니다.'}
+        handleOnclick={refresh}
+      />
+    </Container>
+  );
+
   return (
-    <GridContainer className={cx(Grid, Center)}>
-      {posts &&
-        posts.map((item: UserProfilePostsTypes) => (
-          <GridItemContainer key={item.postId}>
-            <GridItem
-              className={Cursor}
-              key={item.postId}
-              id={item.postId}
-              onTouchEnd={touchImg}
-              src={item.photo}
-            />
-          </GridItemContainer>
-        ))}
-      <Target ref={postRef} />
-    </GridContainer>
+    <>
+      {isError ? (
+        ImgErrorCTA
+      ) : (
+        <GridContainer className={cx(Grid, Center)}>
+          {posts &&
+            posts.map((item: UserProfilePostsTypes) => (
+              <GridItemContainer key={item.postId}>
+                <GridItem
+                  className={Cursor}
+                  key={item.postId}
+                  id={item.postId}
+                  onTouchEnd={touchImg}
+                  onError={handleImgError}
+                  src={item.photo}
+                />
+              </GridItemContainer>
+            ))}
+          <Target ref={postRef} />
+        </GridContainer>
+      )}
+    </>
   );
 };
+
+const Container = styled.div`
+  height: calc(100vh - 460px);
+`;
 
 const GridContainer = styled.div`
   grid-template-columns: repeat(3, 1fr);
