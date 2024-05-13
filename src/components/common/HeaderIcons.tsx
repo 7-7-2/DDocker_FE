8aea598581e4c68283c8a0dc081fa0cf98c78b11
hookor <gerrard1219@gmail.com>
2024-05-13 15:22:13 +0900

    Feat: apply nuread notification icon on Header

--- src/components/common/HeaderIcons.tsx
+++ src/components/common/HeaderIcons.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import HeaderIcon from '@/components/common/HeaderIcon';
 import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
-
-const routeMap = new Map();
-routeMap.set('search', '/search');
-routeMap.set('notification', '/notification');
-
-const icons = ['search', 'notification'];
+import useGetCacheData from '@/hooks/useGetCacheData';
+import { useQuery } from '@tanstack/react-query';
 
 const HeaderIcons = () => {
+  const { data: unreadNotification } = useQuery({
+    queryKey: ['unread'],
+    queryFn: () => {
+      return useGetCacheData('notification', '/unread');
+    }
+  });
+
+  const notification =
+    unreadNotification && unreadNotification.cacheData
+      ? 'unread-notification'
+      : 'notification';
+  const icons = ['search', notification];
+
   return (
     <>
-      {icons.map(item => (
-        <React.Fragment key={item}>
-          <HeaderIcon
-            {...iconPropsGenerator(item)}
-            icon={item}
-          />
-        </React.Fragment>
-      ))}
+      {icons &&
+        icons.map(item => (
+          <React.Fragment key={item}>
+            <HeaderIcon
+              {...iconPropsGenerator(item)}
+              icon={item}
+            />
+          </React.Fragment>
+        ))}
     </>
   );
 };
