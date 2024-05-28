47415a6a2f30321a75ca1aa9216868d53d1280b7
Siwoo Lee <siwoo28@kakao.com>
2024-05-28 18:39:08 +0900

    Fix: deleteAccount Flow

--- src/hooks/MyPage/useHandleAuth.ts
+++ src/hooks/MyPage/useHandleAuth.ts
@@ -1,17 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
+import { useSearchParams } from 'react-router-dom';
 
 import { useDeleteCacheData } from '@/hooks/useDeleteCacheData';
 import { useNavigateTo } from '@/hooks/useNavigateTo';
-import { deleteUserAccount } from '@/api/user';
+import { deleteUserAccount, getSocialAuth } from '@/api/user';
 import { isModalState } from '@/atoms/atoms';
 import { MYPAGE_TEXTS } from '@/constants/profile';
 import { useCloudStorage } from '@/hooks/useCloudStorage';
 import { useCachedUserInfo } from '@/hooks/useCachedUserInfo';
+import useGetCacheData from '@/hooks/useGetCacheData';
 
 const { signOutUrls } = MYPAGE_TEXTS;
 
 export const useHandleAuth = () => {
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get('code');
   const [isModal, setIsModal] = useRecoilState(isModalState);
   const [isConfirm, setIsConfirm] = useState(false);
   const goToStart = useNavigateTo('/start/1');
@@ -45,17 +49,27 @@ export const useHandleAuth = () => {
       return;
     }
     if (isModal) {
-      const deleteAccount = async () => {
-        await handleDeleteHistory();
-        await deleteUserAccount();
-        await deleteStorage(userRoute);
-        await deleteFolder(postRoute);
-        await handleSignOut();
+      const getSocialToken = async () => {
+        const social = await useGetCacheData('user', '/social');
+        await getSocialAuth(social.cacheData, true);
       };
-      deleteAccount();
+      getSocialToken();
       return;
     }
   };
+
+  const deleteAccount = async (code: string) => {
+    const social = await useGetCacheData('user', '/social');
+    await deleteUserAccount(social.cacheData, code);
+    await handleDeleteHistory();
+    await deleteStorage(userRoute);
+    await deleteFolder(postRoute);
+    await handleSignOut();
+  };
+
+  useEffect(() => {
+    code && deleteAccount(code);
+  }, []);
 
   return {
     isModal,
