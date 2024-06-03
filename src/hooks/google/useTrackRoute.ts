ab274725e7e3b77d5aba726b8b99364ab4ac8862
hookor <gerrard1219@gmail.com>
2024-06-03 16:09:43 +0900

    Refactor: modify GA script && preconnect assets

--- src/hooks/google/useTrackRoute.ts
+++ src/hooks/google/useTrackRoute.ts
@@ -1,25 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga4';
 
 const useTrackRoute = () => {
   const location = useLocation();
-  const [initialized, setInitialized] = useState(false);
+  const { NODE_ENV } = import.meta.env;
+  const isPro = NODE_ENV === 'production';
 
   useEffect(() => {
-    if (import.meta.env.VITE_GA) {
-      ReactGA.initialize(import.meta.env.VITE_GA);
-      setInitialized(true);
-    }
-  }, []);
-
-  // location 변경 감지시 pageview 이벤트 전송
-  useEffect(() => {
-    if (initialized) {
-      ReactGA.set({ page: location.pathname });
-      ReactGA.send('pageview');
-    }
-  }, [initialized, location]);
+    if (!isPro) return;
+    window.gtag('event', 'pageview', {
+      page_path: location.pathname + location.search + location.hash,
+      page_search: location.search,
+      page_hash: location.hash
+    });
+  }, [location]);
 };
 
 export default useTrackRoute;
