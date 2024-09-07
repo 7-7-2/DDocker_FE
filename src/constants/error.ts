abc849b60d0885cca64656b497f81b5dc0dd750e
Siwoo Lee <siwoo28@kakao.com>
2024-09-07 18:23:45 +0900

    Refactor: imgErrorText constants

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -3,3 +3,9 @@ export const ERROR_TEXTS = {
   text: `죄송합니다. 요청하신 페이지를 찾을 수 없습니다.
   입력하신 주소가 정확한지 다시 한번 확인해주세요.`
 };
+
+export const ERROR_IMG_TEXTS = {
+  img: '이미지를 로드할 수 없습니다.',
+  detailImg: `게시된 이미지를 찾을 수 없습니다.
+게시물 이미지를 수정 및 삭제 하십시오.`
+};
