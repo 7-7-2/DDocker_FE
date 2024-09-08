f5e4b65e27eb120715ed93c92be57b2e5e5c6aa9
Siwoo Lee <siwoo28@kakao.com>
2024-09-08 17:04:13 +0900

    Refactor: postGrid imgerror constants

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -1,3 +1,5 @@
+import PostsGrid from '@/components/profile/PostsGrid';
+
 export const ERROR_TEXTS = {
   title: '페이지를 찾을 수 없습니다.',
   text: `죄송합니다. 요청하신 페이지를 찾을 수 없습니다.
@@ -6,5 +8,6 @@ export const ERROR_TEXTS = {
 
 export const ERROR_IMG_TEXTS = {
   img: '이미지를 로드할 수 없습니다.',
-  detailImg: '게시된 이미지를 찾을 수 없습니다.'
+  detailImg: '게시된 이미지를 찾을 수 없습니다.',
+  postsGrid: '게시물을 불러올 수 없습니다.'
 };
