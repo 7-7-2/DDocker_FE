0cb7bfb8846928a9830426c31c6803b3246fd2aa
Siwoo Lee <siwoo28@kakao.com>
2026-04-03 01:12:25 +0900

    Refactor: add util coffeeInfoFormatter.ts

---
+++ src/utils/coffeeInfoFormatter.ts
@@ -0,0 +1,16 @@
+import { caffeineIntakeTypes } from '@/types/types';
+import { brandMapToKor } from '@/utils/convertBrandName';
+import { customOptionFormmater } from '@/utils/customOptionFormmater';
+
+export const coffeeInfoFormatter = (caffeineIntake: caffeineIntakeTypes) => {
+  const { caffeine, ...rest } = caffeineIntake;
+  const coffeeInfoValues = Object.values(rest);
+  const customOption = coffeeInfoValues.slice(2, 5);
+
+  const coffeeInfo = [
+    brandMapToKor(coffeeInfoValues[0] as string),
+    coffeeInfoValues[1],
+    customOptionFormmater(customOption)
+  ];
+  return { caffeine, coffeeInfo };
+};
