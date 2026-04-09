b5348d79b916cd85fef063f7c2fd5f4fd0fc9a54
Siwoo Lee <siwoo28@kakao.com>
2026-04-09 18:07:27 +0900

    Design: update ConfirmDeleteUserAccount Modal

--- src/components/common/ModalCTA.tsx
+++ src/components/common/ModalCTA.tsx
@@ -2,32 +2,31 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '@/components/common/Button';
-import Icon from '@/components/common/Icon';
 import { BackgroundLayer } from '@/components/post/overlay/BackgroundLayer';
-import { MODAL_CTA_TEXTS } from '@/constants/common';
 import { useVerifyModalCTA } from '@/hooks/useVerifyModalCTA';
-import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
 
 import { Align, Between, Column, Flex } from '@/styles/layout';
-import { Medium, RegistBtn } from '@/styles/styles';
-import { css, cx } from 'styled-system/css';
+import {
+  BtnColorBorderWhite,
+  BtnColorMain,
+  DubbleShortBtn,
+  Medium,
+  Regular
+} from '@/styles/styles';
+import { cx } from 'styled-system/css';
 import { styled } from 'styled-system/jsx';
 
-const { signIn } = MODAL_CTA_TEXTS;
-
 const ModalCTA = ({
-  actionText,
-  text,
-  subBtnText,
+  buttonText,
+  title,
+  description,
   type,
-  isConfirm,
   fn: handleActions
 }: {
-  actionText: string;
-  text: string;
+  buttonText: Array<string>;
+  title: string;
+  description: string;
   type?: string;
-  subBtnText?: string;
-  isConfirm?: boolean;
   fn: React.MouseEventHandler<HTMLButtonElement>;
 }) => {
   const { isModal, setIsModal } = useVerifyModalCTA();
@@ -43,47 +42,27 @@ const ModalCTA = ({
   const confirmDeleteAuth = (
     <ConfirmContainer className={cx(Flex, Between)}>
       <Button
-        text={'돌아가기'}
-        onClick={HandleOnclick}
-        className={cx(RegistBtn, BackBtn)}
+        text={buttonText[0]}
+        onClick={handleActions}
+        className={cx(BtnColorBorderWhite, DubbleShortBtn)}
       />
       <Button
-        text={'탈퇴하기'}
-        onClick={handleActions}
-        className={RegistBtn}
+        text={buttonText[1]}
+        onClick={HandleOnclick}
+        className={cx(BtnColorMain, DubbleShortBtn)}
       />
     </ConfirmContainer>
-  );
-
-  const DefalutBtn = (
-    <Button
-      text={actionText}
-      onClick={handleActions}
-      className={RegistBtn}
-    />
   );
 
   return (
     isModal && (
       <BackgroundLayer onClick={HandleOnclick}>
-        <ModalContainer>
-          <Container className={cx(Column, Between, Align)}>
-            <TextContainer
-              className={cx(
-                Column,
-                Align,
-                !isConfirm ? OnelineText : undefined
-              )}>
-              <Icon {...iconPropsGenerator('ddocker-modal', '60')} />
-              <Text className={Medium}>{text}</Text>
-            </TextContainer>
-            {!isConfirm ? DefalutBtn : confirmDeleteAuth}
-          </Container>
-          {!type && (
-            <SubBtn onClick={HandleOnclick}>
-              {subBtnText || signIn.subBtn}
-            </SubBtn>
-          )}
+        <ModalContainer className={cx(Column, Between, Align)}>
+          <TextContainer className={cx(Column, Align)}>
+            <Text className={Medium}>{title}</Text>
+            <Description className={Regular}>{description}</Description>
+          </TextContainer>
+          {confirmDeleteAuth}
         </ModalContainer>
       </BackgroundLayer>
     )
@@ -93,8 +72,11 @@ const ModalCTA = ({
 const ModalContainer = styled.div`
   position: absolute;
   width: 300px;
-  height: 248px;
+  height: 198px;
   margin: auto;
+  background-color: #ffffff;
+  padding: 30px 16px 16px 16px;
+  border-radius: 16px;
   top: 0;
   bottom: 0;
   left: 0;
@@ -102,20 +84,16 @@ const ModalContainer = styled.div`
   z-index: 999;
 `;
 
-const Container = styled.div`
-  width: 300px;
-  height: 216px;
-  padding: 16px;
-  border-radius: 16px;
-  background-color: #ffffff;
-`;
-
 const TextContainer = styled.div`
   gap: 6px;
 `;
 
-const OnelineText = css`
-  gap: 16px;
+const Description = styled.div`
+  font-size: var(--font-sizes-sm);
+  line-height: 22px;
+  color: var(--colors-mid-grey);
+  text-align: center;
+  white-space: pre-wrap;
 `;
 
 const Text = styled.div`
@@ -124,24 +102,9 @@ const Text = styled.div`
   text-align: center;
 `;
 
-const SubBtn = styled.button`
-  position: absolute;
-  width: 300px;
-  color: #fff;
-  text-decoration: underline;
-  bottom: 0;
-  font-size: var(--font-sizes-sm);
-`;
-
 const ConfirmContainer = styled.div`
   width: 100%;
   gap: 8px;
-`;
-
-const BackBtn = css`
-  color: var(--colors-mid-grey);
-  background-color: #fff;
-  border: 1px solid var(--colors-btn-grey);
 `;
 
 export default ModalCTA;
