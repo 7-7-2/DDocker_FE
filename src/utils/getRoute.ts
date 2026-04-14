fa0f82232e23b4c058cd5072ab96fc1b629cc9c5
Siwoo Lee <siwoo28@kakao.com>
2026-04-14 15:32:52 +0900

    Refactor: util getRoute.ts

---
+++ src/utils/getRoute.ts
@@ -0,0 +1,10 @@
+export const pathMap = new Map();
+pathMap.set('', 'home');
+pathMap.set('posts', 'feed');
+pathMap.set('coffee', 'stats');
+pathMap.set('profile', 'my');
+
+export const routeMap = new Map();
+routeMap.set('home', '/');
+routeMap.set('feed', '/posts');
+routeMap.set('stats', '/coffee');
