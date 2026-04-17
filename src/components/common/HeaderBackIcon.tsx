4ed67d3d5810ff1435c87c3774715b386626129b
Siwoo Lee <siwoo28@kakao.com>
2026-04-17 22:29:15 +0900

    Refactor: useSmartBack.ts

--- src/components/common/HeaderBackIcon.tsx
+++ src/components/common/HeaderBackIcon.tsx
@@ -1,18 +1,10 @@
-import { useRecoilValue } from 'recoil';
-
 import Icon from '@/components/common/Icon';
-
-import { activeState } from '@/atoms/atoms';
-import { useNavigateTo } from '@/hooks/useNavigateTo';
 import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
-import { routeMap } from '@/utils/getRoute';
+import { useSmartBack } from '@/hooks/post/useSmartBack';
 
 const HeaderBackIcon = () => {
-  const footerActiveState = useRecoilValue(activeState);
-  const myPage = footerActiveState === 'my';
-  const navigateTo = myPage
-    ? useNavigateTo('-1')
-    : useNavigateTo(routeMap.get(footerActiveState));
+  const { navigateTo } = useSmartBack();
+
   const navTofooterState = () => {
     navigateTo();
   };
