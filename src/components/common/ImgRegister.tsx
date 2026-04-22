860cc4b22993352c13a03223b7b233ec47583072
Siwoo Lee <siwoo28@kakao.com>
2026-04-22 23:54:05 +0900

    Design: post register Img input

--- src/components/common/ImgRegister.tsx
+++ src/components/common/ImgRegister.tsx
@@ -1,10 +1,12 @@
 import Icon from '@/components/common/Icon';
-import { FlexCenter } from '@/styles/layout';
-import { MarginT6, Spinner } from '@/styles/styles';
-import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
-import { styled } from 'styled-system/jsx';
-import { ImgRegisterProps } from '@/types/types';
+
 import { useImageCropper } from '@/hooks/post/useImageCropper';
+import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
+import { ImgRegisterProps } from '@/types/types';
+
+import { styled } from 'styled-system/jsx';
+import { Spinner } from '@/styles/styles';
+import { Flex, FlexCenter } from '@/styles/layout';
 
 const ImgRegister = ({
   setImageUrl,
@@ -31,6 +33,20 @@ const ImgRegister = ({
     <>
       {imageUrl && !isLoadingImg && (
         <PostImgContainer>
+          <ImgEditBtnIcons className={Flex}>
+            <BtnItem>
+              <Icon {...iconPropsGenerator('regist-photo-change', '40')} />
+              <EditBtn
+                ref={fileInputRef}
+                type="file"
+                accept="image/*"
+                onChange={handleFileChange}
+              />
+            </BtnItem>
+            <BtnItem onClick={resetImage}>
+              <Icon {...iconPropsGenerator('regist-photo-delete', '40')} />
+            </BtnItem>
+          </ImgEditBtnIcons>
           <Img
             src={imageUrl}
             alt="posted coffee"
@@ -68,17 +84,40 @@ const RegistPhoto = styled.label`
 `;
 
 const PostImgContainer = styled.div`
-  width: 106px;
-  height: 106px;
-  height: auto;
+  height: 100%;
   aspect-ratio: 1;
   border-radius: 10px;
   overflow: hidden;
+  position: relative;
+`;
+
+const ImgEditBtnIcons = styled.div`
+  position: absolute;
+  right: 12px;
+  bottom: 12px;
+  z-index: 3;
+  gap: 10px;
+`;
+
+const BtnItem = styled.div`
+  position: relative;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
+const EditBtn = styled.input`
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  border-radius: 50%;
 `;
 
 const Img = styled.img`
-  width: 106px;
-  height: 106px;
+  height: 100%;
+  width: 100%;
   object-fit: 'cover';
 `;
 
