0b3669efffbe791f01931f991b33e0265fbae36c
Siwoo Lee <siwoo28@kakao.com>
2026-05-01 16:26:09 +0900

    Feat: search api

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,12 +1,20 @@
 import { baseInstance } from '@/api/axiosInterceptor';
+import Search from '@/pages/Page-Search';
 
-// 1. 유저 검색
-export const getSearchUser = async (nickname: string) => {
-  const res = await baseInstance.get(`/search?q=${nickname}`).catch(e => {
-    console.log(e);
-  });
+// 1. 검색
+export const getSearchUser = async (
+  nickname: string,
+  type: string,
+  sort: string
+) => {
+  const res = await baseInstance
+    .get(`/search?q=${nickname}&type=${type}&sort=${sort}`)
+    .catch(e => {
+      console.log(e);
+    });
   return res && res.data;
 };
+
 // 2. 유저 더보기
 export const getSearchMoreUser = async (
   nickname: string,
@@ -16,8 +24,36 @@ export const getSearchMoreUser = async (
     const res = await baseInstance.get('/search', {
       params: {
         q: nickname,
+        type: 'user',
         cursor: cursor,
         limit: 10 // Increase limit for "more" view
+      }
+    });
+    const resData = res.data.data;
+    return {
+      data: resData.results,
+      next: resData.nextCursor
+    };
+  } catch (e) {
+    console.log(e);
+    return { data: [], next: null };
+  }
+};
+
+// 3. 게시글 더보기
+export const getSearchMorePost = async (
+  search: string,
+  sort: string,
+  cursor?: string | null
+) => {
+  try {
+    const res = await baseInstance.get('/search', {
+      params: {
+        q: search,
+        type: 'post',
+        sort: sort,
+        cursor: cursor,
+        limit: 10
       }
     });
     const resData = res.data.data;
