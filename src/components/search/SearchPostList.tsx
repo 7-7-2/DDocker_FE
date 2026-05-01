0bc3cd9671dd63342d3736d1dfbd91a4e44fbf76
Siwoo Lee <siwoo28@kakao.com>
2026-05-01 21:11:31 +0900

    Refactor: Extract SortBtn component

--- src/components/search/SearchPostList.tsx
+++ src/components/search/SearchPostList.tsx
@@ -1,17 +1,15 @@
-import Icon from '@/components/common/Icon';
+import { useState } from 'react';
+
 import PostItem from '@/components/common/PostItem';
 import SearchMore from '@/components/search/SearchMore';
 import SearchMoreList from '@/components/search/SearchMoreList';
 
 import { useSetHistory } from '@/hooks/search/useSetHistory';
-import { iconPropsGenerator } from '@/utils/iconPropsGenerator';
 import { SearchPostListProps } from '@/types/types';
 import { SEARCH_TEXTS } from '@/constants/search';
 
 import { styled } from 'styled-system/jsx';
-import { Flex } from '@/styles/layout';
-import { Medium } from '@/styles/styles';
-import { useState } from 'react';
+import SortBtn from '@/components/common/SortBtn';
 
 const SearchPostList = ({
   posts,
@@ -22,6 +20,7 @@ const SearchPostList = ({
   const { mutateSearchText } = useSetHistory();
   const [loadMore, setLoadMore] = useState(false);
   const [isLabelText, setIsLabelText] = useState(false);
+
   const handleSortBtn = () => {
     clickSortBtn();
     setIsLabelText(!isLabelText);
@@ -39,15 +38,9 @@ const SearchPostList = ({
   return (
     <>
       <SortBtn
-        className={Flex}
-        onClick={handleSortBtn}>
-        <SortText className={Medium}>
-          {!isLabelText
-            ? SEARCH_TEXTS.sortOption[0]
-            : SEARCH_TEXTS.sortOption[1]}
-        </SortText>
-        <Icon {...iconPropsGenerator('sort', '20')} />
-      </SortBtn>
+        btnState={isLabelText}
+        handleOnClick={handleSortBtn}
+      />
       {posts.map(item => (
         <div
           onClick={handleOnclick}
@@ -77,16 +70,6 @@ const SearchPostList = ({
   );
 };
 
-const SortText = styled.span`
-  font-size: var(--font-sizes-sm);
-  line-height: 20px;
-  color: var(--colors-main-dark);
-`;
-const SortBtn = styled.button`
-  gap: 2px;
-  margin: 20px 0 8px 0;
-  justify-self: end;
-`;
 const BtnArea = styled.div`
   margin: 50px 0;
 `;
