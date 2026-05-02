9c0e1981666ede21055de338aaef555ca9a6466f
Siwoo Lee <siwoo28@kakao.com>
2026-05-02 14:16:04 +0900

    Feat: getAnchorPosition.ts for AlretBubble.tsx

--- src/components/common/AlertBubble.tsx
+++ src/components/common/AlertBubble.tsx
@@ -7,16 +7,19 @@ import { InputFontSm } from '@/styles/styles';
 
 const AlertBubble = ({
   type = null,
+  location,
   message
 }: {
   type?: string | null;
+  location?: number;
   message: JSX.Element | string;
 }) => {
+  const tailPosition =
+    type === 'trends' ? { left: `${location}px` } : { right: '20%' };
+
   return (
     <AlertBubbleContainer className={cx(Align)}>
-      {type !== null && (
-        <AlertBubbleTail className={type === 'trends' ? Left : Right} />
-      )}
+      {type !== null && <AlertBubbleTail style={tailPosition} />}
       <Icon {...iconPropsGenerator('message', '15')} />
       <MessageText className={InputFontSm}>{message}</MessageText>
     </AlertBubbleContainer>
@@ -37,19 +40,18 @@ const AlertBubbleTail = styled.div`
   position: absolute;
   width: 20px;
   height: 20px;
+  bottom: -7px;
   transform: rotate(45deg);
   background-color: var(--colors-main);
   border-radius: 0 0 4px 0;
 `;
 
 const Left = css`
-  left: 7%;
-  bottom: -7px;
+  left: 16%;
 `;
 
 const Right = css`
   right: 20%;
-  bottom: -7px;
 `;
 
 const MessageText = styled.div`
