6b4cc8341fdfa39ef103716c2cc4b19673b70adb
Siwoo Lee <siwoo28@kakao.com>
2026-05-06 13:54:01 +0900

    Refactor: extract sortBtn.tsx

--- src/components/search/SearchPostList.tsx
+++ src/components/search/SearchPostList.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import SortBtn from '@/components/common/SortBtn';
 import PostItem from '@/components/common/PostItem';
 import SearchMore from '@/components/search/SearchMore';
 import SearchMoreList from '@/components/search/SearchMoreList';
@@ -9,7 +10,6 @@ import { SearchPostListProps } from '@/types/types';
 import { SEARCH_TEXTS } from '@/constants/search';
 
 import { styled } from 'styled-system/jsx';
-import SortBtn from '@/components/common/SortBtn';
 
 const SearchPostList = ({
   posts,
@@ -36,41 +36,52 @@ const SearchPostList = ({
   };
 
   return (
-    <>
+    <Container>
       <SortBtn
         btnState={isLabelText}
         handleOnClick={handleSortBtn}
       />
-      {posts.map(item => (
-        <div
-          onClick={handleOnclick}
-          key={item.postId}>
-          <PostItem
-            item={item}
+      <PostsContainer>
+        {posts.map(item => (
+          <div
+            onClick={handleOnclick}
+            key={item.postId}>
+            <PostItem
+              item={item}
+              search={search}
+            />
+          </div>
+        ))}
+        {posts.length == 5 && !loadMore && (
+          <BtnArea>
+            <SearchMore onClick={handleSearchMore} />
+          </BtnArea>
+        )}
+        {loadMore && (
+          <SearchMoreList
             search={search}
+            type={SEARCH_TEXTS.tabs[0]}
+            sort={
+              !isLabelText
+                ? SEARCH_TEXTS.type.sort[0]
+                : SEARCH_TEXTS.type.sort[1]
+            }
+            initialCursor={initialCursor || null}
           />
-        </div>
-      ))}
-      {posts.length == 5 && !loadMore && (
-        <BtnArea>
-          <SearchMore onClick={handleSearchMore} />
-        </BtnArea>
-      )}
-      {loadMore && (
-        <SearchMoreList
-          search={search}
-          type={SEARCH_TEXTS.tabs[0]}
-          sort={
-            !isLabelText ? SEARCH_TEXTS.type.sort[0] : SEARCH_TEXTS.type.sort[1]
-          }
-          initialCursor={initialCursor || null}
-        />
-      )}
-    </>
+        )}
+      </PostsContainer>
+    </Container>
   );
 };
+const Container = styled.div`
+  margin-top: 20px;
+`;
 
+const PostsContainer = styled.div`
+  margin-top: 6px;
+`;
 const BtnArea = styled.div`
   margin: 50px 0;
 `;
+
 export default SearchPostList;
