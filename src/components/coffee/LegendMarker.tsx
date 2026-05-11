40d1bc9b58d910aae4b28837f32c7338e280d55f
Siwoo Lee <siwoo28@kakao.com>
2026-05-11 19:40:09 +0900

    Refactor: extract LegendMarker.tsx

---
+++ src/components/coffee/LegendMarker.tsx
@@ -0,0 +1,27 @@
+import { COFFEE_CALENDAR_TEXTS } from '@/constants/coffee';
+
+import { styled } from 'styled-system/jsx';
+import { Excessive, Healthy, Recommended } from '@/styles/styles';
+const LegendMarker = ({ legend }: { legend: string }) => {
+  return (
+    <>
+      <Marker
+        className={
+          legend === COFFEE_CALENDAR_TEXTS.legend[0].className
+            ? Healthy
+            : legend === COFFEE_CALENDAR_TEXTS.legend[1].className
+              ? Recommended
+              : Excessive
+        }
+      />
+    </>
+  );
+};
+
+const Marker = styled.div`
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+`;
+
+export default LegendMarker;
