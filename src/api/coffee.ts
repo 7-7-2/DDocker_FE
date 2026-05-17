c748cc9b7019348776eb6a9a1a31a5cccd4a31c9
Siwoo Lee <siwoo28@kakao.com>
2026-05-17 23:37:48 +0900

    Feat: add caffeine intake delete api

--- src/api/coffee.ts
+++ src/api/coffee.ts
@@ -19,3 +19,21 @@ export const getCoffeeCaledar = async (activeMonth: string) => {
     });
   return res && res.data.data;
 };
+
+// 3. 카페인 섭취 내역 - 게시물로 등록되었을 경우 게시물 ID 조회
+export const getPostId = async (caffeineId: string) => {
+  const res = await authInstance
+    .get(`/caffeine/intake/${caffeineId}/post`)
+    .catch(e => {
+      console.log(e);
+    });
+  return res && res.data.data;
+};
+
+// 4. 카페인 섭취 내역 - 삭제
+export const deleteCaffeineIntake = async (caffeineId: string) => {
+  await authInstance.delete(`/caffeine/intake/${caffeineId}`).catch(e => {
+    console.log(e);
+  });
+  return;
+};
