c4f235877950a9ba0f9b1e179b848274e9dce887
Siwoo Lee <siwoo28@kakao.com>
2026-05-20 20:59:20 +0900

    Feat: add coffeSum data

---
+++ src/components/coffee/CoffeeSum.tsx
@@ -0,0 +1,45 @@
+import { COFFEE_ANALYSIS_TEXTS } from '@/constants/coffee';
+
+import { cx } from 'styled-system/css';
+import { styled } from 'styled-system/jsx';
+import { Center, Column, Grid } from '@/styles/layout';
+import { Bold, BorderRight } from '@/styles/styles';
+
+const { coffeeSum, unit } = COFFEE_ANALYSIS_TEXTS;
+const CoffeeSum = ({ data }: { data: number[] }) => {
+  return (
+    <Container className={Grid}>
+      {coffeeSum.text.map((item, idx) => (
+        <div
+          key={item}
+          className={cx(
+            Column,
+            Center,
+            coffeeSum.text.length - 1 !== idx && BorderRight
+          )}>
+          <span className={Bold}>
+            {data[idx]}
+            {coffeeSum.text.length - 1 !== idx ? unit.cup : unit.day}
+          </span>
+          <Text>{item}</Text>
+        </div>
+      ))}
+    </Container>
+  );
+};
+const Container = styled.div`
+  height: 80px;
+  grid-template-columns: 1fr 1fr 1fr;
+  border-radius: 10px;
+  padding: 20px 0;
+  margin-bottom: 26px;
+  font-size: var(--font-sizes-base);
+  line-height: 24px;
+  background-color: var(--colors-main);
+  color: #fff;
+`;
+
+const Text = styled.span`
+  font-size: var(--font-sizes-xs);
+`;
+export default CoffeeSum;
